92efef5c0d44c1d8de96415272f15bd3972d1b57
Prabesh <prabesh.bista369@gmail.com>
2022-01-08 21:35:47 +0545

    primitive and non-primitive data types

--- Start1.java
+++ Start1.java
@@ -1,9 +1,23 @@
 public class Start1{
 public static void main(String[] args){
-        String name="Prabesh Bista";
-        int age =20;
-        System.out.println(age);
+        // byte age=30;
+        // int phone=1234567890;
+        // long phone2=123456789011L;
+        // float pie=3.14F;
+        // double pie1=1.2333;
+        // char ch='@';
+        // boolean isAdult=false;
+        
 
-        System.out.println(name);
     }
 }
+
+//Primitive data-type
+//byte -1[-128 to 127]
+//short -2
+//int -4
+//long -8
+// float -4
+//double -8 
+//clhar -2 a,b,c,d
+// boolean -1 true/false
