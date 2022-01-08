a930520093fd33590348f2cf885866cfd5dc23b1
Prabesh <prabesh.bista369@gmail.com>
2022-01-08 21:39:58 +0545

    primitive and non-primitive data types

--- nonP.java
+++ nonP.java
@@ -2,7 +2,15 @@ public class nonP {
     public static void main(String[] args){
         //Non -Primitive types
         String name="Prabesh";
+        // returns the length of the String
         System.out.println(name.length());
+        //concat() method joins another string to the existing string
+        System.out.println(name.concat(" Bista"));
+        //CharAt() returns the character at the given index
+        System.out.println(name.charAt(2));
+        // replace() method replace the given char
+        System.out.println(name.replace("P", "S"));
+        
 
     }
 }
