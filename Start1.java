4612ed3260e01d025e6f3be10415cb545899b43d
Prabesh <prabesh.bista369@gmail.com>
2022-01-08 21:10:40 +0545

    string,int

--- Start1.java
+++ Start1.java
@@ -1,7 +1,9 @@
-
-
 public class Start1{
-    public static void main(String[] args){
-        System.out.println("My name is Prabesh Bista");
+public static void main(String[] args){
+        String name="Prabesh Bista";
+        int age =20;
+        System.out.println(age);
+
+        System.out.println(name);
     }
 }
