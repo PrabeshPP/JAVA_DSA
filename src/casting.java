31e7b6b312eed7ab0e9db89f0ed92081a24c310f
Prabesh <prabesh.bista369@gmail.com>
2022-01-10 21:41:29 +0545

    Casting basic

---
+++ src/casting.java
@@ -0,0 +1,14 @@
+public class casting {
+    public static void main(String[] args){
+        //casting 
+       double price=100.00;
+       double finalPrice=price+(price*0.13);
+       System.out.println(finalPrice);
+
+       int p=112;
+      double fPrice=p+13.0;
+      System.out.println(fPrice);
+
+
+    }
+}
