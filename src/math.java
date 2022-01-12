853f1c1544107bb4998ad077772fa5a1b2cc4799
Prabesh <prabesh.bista369@gmail.com>
2022-01-12 09:21:11 +0545

    Math()

--- src/math.java
+++ src/math.java
@@ -5,6 +5,15 @@ public class math {
         double y=-10;
         double z=Math.max(x, y);
         System.out.println("The highest number is "+z);
+        // abs() method in math
+        int negNumber=-100230;
+        int posNumber=Math.abs(negNumber);
+        System.out.println("Your negative number has been converted to "+posNumber);
+
+        //Sqrt() method returns the square root of the given positive number
+        int value=(int)Math.sqrt(169);
+        System.out.println(value);
+        
 
     }
 }
