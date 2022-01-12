7c7d3da9c3591d49a51a242c1fca3a8720846032
Prabesh <prabesh.bista369@gmail.com>
2022-01-12 08:55:20 +0545

    input in java

--- src/user_input.java
+++ src/user_input.java
@@ -3,9 +3,18 @@ import java.util.Scanner;
 public class user_input {
     public static void main(String[] args) {
        Scanner  scanner=new Scanner(System.in);
+       // Taking String as an input
+       System.out.println("What is  your Name ?");
+       String name=scanner.nextLine();
+       System.out.println("Hello "+name);
+        //Taking integer as an input
        System.out.println("What is your age?");
        int age=scanner.nextInt();
-       System.out.println("Your age is "+age);;
+       System.out.println("Your age is "+age);
+       //Taking double as an input
+       System.out.println("What is your monthly salary ?");
+       double salary=scanner.nextDouble();
+       System.out.println("You monthly salary is "+salary);
        scanner.close();
     }
     
