bb768c57d31da1e31652733cad1b6c8cfbc4285c
Prabesh <prabesh.bista369@gmail.com>
2022-01-12 10:23:07 +0545

    logical Operator() ----->

--- src/logical_oper.java
+++ src/logical_oper.java
@@ -1,3 +1,5 @@
+
+
 public class logical_oper {
     public static void main(String[] args) {
         // logical operators = used to connect two or more expressions
@@ -9,14 +11,19 @@ public class logical_oper {
         int x=2;
         int y=20;
 
-
+        // && And Operator
         if(x==2 && x>y){
             System.out.println("Two expressions are tue!");
+                // || OR Operator
         }else if(x==2 || x>y){
             System.out.println("One of the expression is true !");
+            // != NOT Equal to Operator
         }else if(x!=y){
             System.out.println("The not equla express is true");
         }
+
+        
+
 
 
     }
