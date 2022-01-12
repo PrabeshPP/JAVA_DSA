1ef13e0dadaa73cf2dbdbbca275be0aa3f9d4fb0
Prabesh <prabesh.bista369@gmail.com>
2022-01-12 09:48:38 +0545

    random() ---->>>

---
+++ src/random.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+
+public class random {
+    public static void main(String[] args) {
+        Random random=new Random();
+        System.out.println(random.nextInt(1000));
+    }
+    
+}
