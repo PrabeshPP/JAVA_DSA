6ffe5e4238ea94e0f055e10379505d975277bf9b
Prabesh <prabesh.bista369@gmail.com>
2022-01-12 09:13:49 +0545

    gui

--- src/gui.java
+++ src/gui.java
@@ -4,9 +4,12 @@ import javax.swing.JOptionPane;
 public class gui {
     public static void main(String[] args) {
         String name=JOptionPane.showInputDialog("Enter your name");
-        JOptionPane.showMessageDialog(null, "Hello"+name);
+        JOptionPane.showMessageDialog(null, "Hello "+name);
 
        int age=Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
        JOptionPane.showMessageDialog(null,"Your age is "+age);
+
+       double pie=Double.parseDouble(JOptionPane.showInputDialog(null,"Enter the value of Pie"));
+       JOptionPane.showMessageDialog(null,"The value of Pie is "+pie);
     }
 }
