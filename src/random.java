3374f1f6008987192df442bed2bcf24c616bb4f8
Prabesh <prabesh.bista369@gmail.com>
2022-01-12 09:48:42 +0545

    random() ---->>>

--- src/random.java
+++ src/random.java
@@ -3,7 +3,19 @@ import java.util.Random;
 public class random {
     public static void main(String[] args) {
         Random random=new Random();
-        System.out.println(random.nextInt(1000));
+        //Generating random integer
+
+        int x=random.nextInt(6)+1;
+        System.out.println(x);
+
+        //Generating random double 
+        double y=random.nextDouble(40);
+        System.out.println(y);
+
+        //Generating random boolean
+        boolean b=random.nextBoolean();
+        System.out.println(b);
+
     }
     
 }
