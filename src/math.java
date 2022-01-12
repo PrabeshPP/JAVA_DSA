0bece97a8bac30c47cc3641b01027fa3624117bc
Prabesh <prabesh.bista369@gmail.com>
2022-01-12 09:21:06 +0545

    Math()

---
+++ src/math.java
@@ -0,0 +1,10 @@
+public class math {
+    public static void main(String[] args) {
+        // finding the hypotenuse of the triangle
+        double x=3.14;
+        double y=-10;
+        double z=Math.max(x, y);
+        System.out.println("The highest number is "+z);
+
+    }
+}
