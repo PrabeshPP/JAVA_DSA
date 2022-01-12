43f0b3edc07c6263bbcb27805ed7f8f5880f45ab
Prabesh <prabesh.bista369@gmail.com>
2022-01-12 09:40:06 +0545

    finding hypotenuse--->

--- src/h.java
+++ src/h.java
@@ -1,18 +1,17 @@
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class h {
     public static void main(String[] args) {
         //Finding the hypotenuse of Triangle
+
         double p,b,h;
-        System.out.println("Hello User! This program was created to find the hypotenuse of the triangle");
-        Scanner scanner=new Scanner(System.in);
-        System.out.println("Enter the size of the base of triangle :");
-        b=scanner.nextDouble();
-        System.out.println("Enter the the length of the perpendicualr of triangle :");
-        p=scanner.nextDouble();
+        JOptionPane.showMessageDialog(null,"Hello User! This program was created to find the hypotenuse of the triangle");
+        b=Double.parseDouble(JOptionPane.showInputDialog("Enter the base of triangle :" ));
+        p=Double.parseDouble(JOptionPane.showInputDialog(null,"Enter the perpendicular of the triangle :"));
         h=Math.sqrt(Math.pow(b, 2)+Math.pow(p, 2));
-        System.out.println("The length of the hypotenuse of the triangle is "+h);
-        scanner.close();
+        JOptionPane.showMessageDialog(null,"The hypotenuse of the triangle is "+h);
+        
+        
     }
     
 }
