327c7f51f5357f92adce6d2309a594295f3138b0
Prabesh <prabesh.bista369@gmail.com>
2022-01-13 08:57:41 +0545

    for loop(),nested loop()

--- src/loop.java
+++ src/loop.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 public class loop {
     public static void main(String[] args) {
-        // System.out.println("This is a program to display the multiplication table of given number :)");
-        // Scanner scanner=new Scanner(System.in);
-        // System.out.println("Enter the number of whhich you want to display the multiplication table :");
-        // int number=scanner.nextInt();
-        // for(int i=1;i<=10;i++){
-        //     System.out.println(i +"*"+8 +"=" +(i*number));
-        // }
-        // scanner.close();
+        System.out.println("This is a program to display the multiplication table of given number :)");
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the number of whhich you want to display the multiplication table :");
+        int number=scanner.nextInt();
+        for(int i=1;i<=10;i++){
+            System.out.println(i +"*"+8 +"=" +(i*number));
+        }
+        scanner.close();
 
         for(int i=0;i<4;++i){
             System.out.println();
