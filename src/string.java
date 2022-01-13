28003f3a2dc43bb180629da063cec5c07a2289f0
Prabesh <prabesh.bista369@gmail.com>
2022-01-13 09:39:52 +0545

    String method()

---
+++ src/string.java
@@ -0,0 +1,9 @@
+public class string {
+    public static void main(String[] args) {
+        String country="Nepal";
+        //comparing the string
+        boolean result=country.equals("Nepal");
+        System.out.println(result);
+    }
+    
+}
