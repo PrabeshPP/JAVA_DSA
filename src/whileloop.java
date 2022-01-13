b85589bb2f03ffef91efaa7c4748a20864999f74
Prabesh <prabesh.bista369@gmail.com>
2022-01-13 08:44:55 +0545

    while loop

--- src/whileloop.java
+++ src/whileloop.java
@@ -5,6 +5,8 @@ public class whileloop {
         Scanner scanner=new Scanner(System.in);
         System.out.println("Enter your name :");
         String name=" ";
+
+        //Using while loop to check whether the name is blank or not!
         while(name.isBlank()){
             System.out.println("Enter your name again :");
             name=scanner.nextLine();
