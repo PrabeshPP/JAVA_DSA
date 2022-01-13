7e9fad489e57fdcec46ba4f7bddd4515fb7f90ae
Prabesh <prabesh.bista369@gmail.com>
2022-01-13 08:57:18 +0545

    for loop(),nested loop()

---
+++ src/loop.java
@@ -0,0 +1,10 @@
+public class loop {
+    public static void main(String[] args) {
+        
+        //Displaying the table of 8
+        for(int i=;i<=10;++i){
+            System.out.println(i +"*"+8 +"=" +(i*8));
+        }
+    }
+    
+}
