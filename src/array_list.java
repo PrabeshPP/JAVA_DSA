b6731409e593018baa6443b511ebd346108b78d5
Prabesh <prabesh.bista369@gmail.com>
2022-01-14 09:44:43 +0545

    array_List

--- src/array_list.java
+++ src/array_list.java
@@ -7,9 +7,11 @@ public class array_list {
         number.add(10);
         number.add(20);
         number.add(39);
-
+        // set() method sets the value at index 0 "229"
         number.set(0, 229);
+        // remove() methods remove the element from the given index
         number.remove(2);
+        // It removes all the elements from the list
         number.clear();
         for(int i=0;i<number.size();++i){
             System.out.println(number.get(i));
