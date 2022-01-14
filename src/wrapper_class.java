ff7087631d26bb6297c0c44d6d5cc308c43549ba
Prabesh <prabesh.bista369@gmail.com>
2022-01-14 09:37:45 +0545

    wrapper class

---
+++ src/wrapper_class.java
@@ -0,0 +1,13 @@
+public class wrapper_class {
+    public static void main(String[] args) {
+        Boolean a=true;
+        Character b='a';
+        Integer c=200;
+        Double d=23.46;
+        String e="bro";
+        if(a==true){
+            System.out.println("This is true");
+        }
+    }
+    
+}
