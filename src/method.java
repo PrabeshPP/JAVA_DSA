38a6501e13842613bd71a09c5e07419e7e5448db
Prabesh <prabesh.bista369@gmail.com>
2022-01-14 10:14:17 +0545

    method in java()

---
+++ src/method.java
@@ -0,0 +1,12 @@
+public class method {
+    public static void main(String[] args) {
+        
+        // metohod= a block of code that is executed whenever it is called;
+        hello();
+    }
+
+    static void hello(){
+        System.out.println("Hello Prabesh!");
+    }
+    
+}
