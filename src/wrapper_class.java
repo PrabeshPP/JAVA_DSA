f45f18a05ac9629366ddf6f4bd156dfbad25d3ee
Prabesh <prabesh.bista369@gmail.com>
2022-01-14 09:37:49 +0545

    wrapper class

--- src/wrapper_class.java
+++ src/wrapper_class.java
@@ -1,12 +1,15 @@
 public class wrapper_class {
     public static void main(String[] args) {
+        // autoUnboxing
         Boolean a=true;
         Character b='a';
         Integer c=200;
         Double d=23.46;
         String e="bro";
-        if(a==true){
-            System.out.println("This is true");
+        
+        //Unboxing
+        if(b=='a'){
+            System.out.println("Yes,it is the same  character!");
         }
     }
     
