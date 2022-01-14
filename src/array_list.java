5ef3e839d7fbeb85345fbc784b82f1f75c576d52
Prabesh <prabesh.bista369@gmail.com>
2022-01-14 09:53:33 +0545

    array_List,2D Array List

--- src/array_list.java
+++ src/array_list.java
@@ -1,22 +1,21 @@
-import java.lang.management.GarbageCollectorMXBean;
 import java.util.ArrayList;
 
 public class array_list {
     
     public static void main(String[] args) {
-        // ArrayList<Integer> number=new ArrayList<Integer>();
-        // number.add(10);
-        // number.add(20);
-        // number.add(39);
-        // // set() method sets the value at index 0 "229"
-        // number.set(0, 229);
-        // // remove() methods remove the element from the given index
-        // number.remove(2);
-        // // It removes all the elements from the list
-        // number.clear();
-        // for(int i=0;i<number.size();++i){
-        //     System.out.println(number.get(i));
-        // }
+        ArrayList<Integer> number=new ArrayList<Integer>();
+        number.add(10);
+        number.add(20);
+        number.add(39);
+        // set() method sets the value at index 0 "229"
+        number.set(0, 229);
+        // remove() methods remove the element from the given index
+        number.remove(2);
+        // It removes all the elements from the list
+        number.clear();
+        for(int i=0;i<number.size();++i){
+            System.out.println(number.get(i));
+        }
 
         // 2D array list
             ArrayList<ArrayList<String>> groceryList=new ArrayList<>();
