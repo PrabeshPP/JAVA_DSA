1e5c857efb252e08f8a02571b884c3ac9e5c59db
Prabesh <prabesh.bista369@gmail.com>
2022-01-14 10:20:47 +0545

    overloaded method() ----->>>

--- src/overloadedMethod.java
+++ src/overloadedMethod.java
@@ -1,6 +1,13 @@
 public class overloadedMethod {
     public static void main(String[] args) {
-        
+        int sum=add(1, 2);
+        System.out.println(sum);
+        sum=add(10, 20, 30);
+        System.out.println(sum);
+        sum=add(10, 20, 30, 40);
+        System.out.println(sum);
+
+
     }
 
     static int add(int x,int y){
@@ -9,9 +16,11 @@ public class overloadedMethod {
     }
 
     static int add(int x,int y,int z){
+        System.out.println("This is Overloaded method #2");
         return x+y+z;
     }
     static int add(int x,int y ,int z ,int a){
+        System.out.println("This is Overloaded method #3");
         return x+y+z+a;
 
     }
