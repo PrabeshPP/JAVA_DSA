125f7a38a5b9d9dedadb45729cf00a8238400d64
Prabesh <prabesh.bista369@gmail.com>
2022-01-16 10:42:31 +0545

    overloaded constructor () ------>>>>

---
+++ src/Pizza.java
@@ -0,0 +1,17 @@
+public class Pizza {
+   final String bread;
+   final String sauce;
+   final String cheese;
+   final  String topping;
+
+    Pizza(String bread,String sauce,String cheese,String topping){
+        this.bread=bread;
+        this.sauce=sauce;
+        this.cheese=cheese;
+        this.topping=topping;
+        
+    }
+
+    
+    
+}
--- src/main1.java
+++ src/main1.java
@@ -1,7 +1,14 @@
 public class main1 {
     public static void main(String[] args) {
         // creating instance of the class Scope()
-        Scope dice=new Scope();
-        dice.roll();
+       Pizza pizza=new Pizza("Thicc crust","tomato","mozzerella","peporani");
+       System.out.println("Here are the ingredients of your pizza!");
+       System.out.println(pizza.bread);
+       System.out.println(pizza.cheese);
+       System.out.println(pizza.sauce);
+       System.out.println(pizza.topping);
+
     }
+
+    
 }
