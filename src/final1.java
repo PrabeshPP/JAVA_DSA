125829a633fe11b8c606dfbfad434cbb7a5e1486
Prabesh <prabesh.bista369@gmail.com>
2022-01-16 09:49:30 +0545

    final keyword ------>>>

---
+++ src/final1.java
@@ -0,0 +1,9 @@
+public class final1 {
+    public static void main(String[] args) {
+        // Final Keyword
+        final double pi;
+        pi=3.14159;
+        System.out.println(pi);
+    }
+    
+}
