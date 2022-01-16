712eb9b7e637c9c97db7fe16913241bfec3e61b1
Prabesh <prabesh.bista369@gmail.com>
2022-01-16 10:02:18 +0545

    OOPS in Java () ----->>>

---
+++ src/car.java
@@ -0,0 +1,15 @@
+public class car {
+    String make="Chevrolet";
+    String model="Corvette";
+    int year=2020;
+    String color="blue";
+    double price= 50000.0;
+
+    void drive(){
+        System.out.println("You drive the car!");
+    }
+
+    void brake(){
+        System.out.println("You pressed the brake of the car!");
+    }
+}
