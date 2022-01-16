2e14bbcbd988f01d76b593e51c4eee304e223947
Prabesh <prabesh.bista369@gmail.com>
2022-01-16 09:55:03 +0545

    oop in Java

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+            // Object = an instance of a class that may contain attributes and methods
+            // example: (phone,desk,computer,coffee,cup)
+
+
+
+
+
+    }
+    
+}
