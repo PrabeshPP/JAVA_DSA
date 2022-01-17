d7813d45a59b62482c01e9f0d7808761dc55f1c0
Prabesh <prabesh.bista369@gmail.com>
2022-01-17 09:53:03 +0545

    Passing obj as an arguments()-->>>

--- src/Garage.java
+++ src/Garage.java
@@ -1,3 +1,6 @@
 public class Garage {
+    void park(car1 car){
+        System.out.println(car.name+" has been parked perfectly in the parking area");
+    }
     
 }
---
+++ src/car1.java
@@ -0,0 +1,7 @@
+public class car1 {
+    String name;
+    car1(String name){
+        this.name=name;
+    }
+    
+}
