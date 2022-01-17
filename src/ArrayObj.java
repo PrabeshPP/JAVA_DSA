088088dd6525a1e9f828b07cc81c047fda2d8bec
Prabesh <prabesh.bista369@gmail.com>
2022-01-17 09:47:27 +0545

    array objects

--- src/ArrayObj.java
+++ src/ArrayObj.java
@@ -1,13 +1,9 @@
 public class ArrayObj {
     public static void main(String[] args) {
-        Food[] fridge=new Food[3];
         Food food1=new Food("pizza");
         Food food2=new Food("hotdog");
         Food food3=new Food("Burger");
-
-        fridge[0]=food1;
-        fridge[1]=food2;
-        fridge[2]=food3;
+        Food[] fridge={food1,food2,food3};
 
         System.out.println(fridge[1].name);
         System.out.println(fridge[0].name);
