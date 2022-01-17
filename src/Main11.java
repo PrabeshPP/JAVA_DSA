c74dcf0899a0f1d26096fa5d22d8fefd2a674479
Prabesh <prabesh.bista369@gmail.com>
2022-01-17 10:27:00 +0545

    inheritance ,extends() ---->>>

---
+++ src/Main11.java
@@ -0,0 +1,7 @@
+public  class Main11 {
+    public static void main(String[] args) {
+        Bicycle bicycle=new Bicycle();
+        System.out.println(bicycle.speed);
+    }
+    
+}
---
+++ src/Vehicle.java
@@ -0,0 +1,11 @@
+public class Vehicle{
+    double speed;
+    void go(){
+        System.out.println("The vehicle is moving.");
+    }
+
+    void stop(){
+        System.out.println("The vehicle is stopped");
+    }
+
+}
