2d8ab2fdf1af0834eca2d1c369dd1ec036fda988
Prabesh <prabesh.bista369@gmail.com>
2022-01-17 21:07:37 +0545

    exploring oops in java () ----->>>>

--- src/obj.java
+++ src/obj.java
@@ -1,8 +1,9 @@
 public class obj {
     public static void main(String[] args) {
         Student prabesh;
-        prabesh=new Student();
+        prabesh=new Student(12,"Prabesh Bista",97);
         
+        System.out.println(prabesh);
         
     }
     
@@ -12,4 +13,15 @@ class Student{
     int rno;
     String name;
     float marks;
+
+    Student(int rno,String name,float marks){
+        this.rno=rno;
+        this.name=name;
+        this.marks=marks;
+
+    }
+public String toString(){
+        String p="Student name is "+name+"roll no. is " +rno+"and he has obtained "+marks+"in physics.";
+       return p;
+}
 }
