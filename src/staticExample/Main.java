bcf941565fc40c67e3df466bc929a7f31adacd04
Prabesh <prabesh.bista369@gmail.com>
2022-01-18 16:55:02 +0545

    Java oops

--- src/staticExample/Main.java
+++ src/staticExample/Main.java
@@ -2,10 +2,10 @@ package staticExample;
 
 public class Main {
     public static void main(String[] args) {
-        Human prabesh=new Human(20, "Prabesh Bista", 10000, false);
-        Human carlo=new Human(41, "carlo", 30000,true);
-        System.out.println(prabesh.name);
-        System.out.println(carlo.name);
+         Human prabesh=new Human(20, "Prabesh Bista", 10000, false);
+         Human carlo=new Human(41, "carlo", 30000,true);
+         System.out.println(prabesh.name);
+         System.out.println(carlo.name);
     }
     
 }
