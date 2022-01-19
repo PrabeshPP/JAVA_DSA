5cae3595827543e11f0f41201c9abf1512c4517c
Prabesh <prabesh.bista369@gmail.com>
2022-01-19 09:40:33 +0545

    static variable

--- src/staticExample/Main.java
+++ src/staticExample/Main.java
@@ -2,10 +2,11 @@ package staticExample;
 
 public class Main {
     public static void main(String[] args) {
-         Human prabesh=new Human(20, "Prabesh Bista", 10000, false);
+         Human prabesh=new Human(20, "Prabesh Bista", 10000,false);
          Human carlo=new Human(41, "carlo", 30000,true);
          System.out.println(prabesh.name);
          System.out.println(carlo.name);
+         System.out.println(Human.population);
     }
     
 }
