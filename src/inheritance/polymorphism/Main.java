7d32ef405ee9f929eb0c5c19c2dc53ff47d50b5a
Prabesh <prabesh.bista369@gmail.com>
2022-01-25 10:44:29 +0545

    Polymorphism in Java

---
+++ src/inheritance/polymorphism/Main.java
@@ -0,0 +1,11 @@
+package inheritance.polymorphism;
+
+public class Main {
+    public static void main(String[] args) {
+        Shapes shapes=new Shapes();
+        Square square=new Square();
+        Circle circle=new Circle();
+        Triangle triangle=new Triangle();
+
+    }
+}
