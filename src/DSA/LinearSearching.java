a3f7215eb813b0909eba5578e0662bbb9b4936e5
Prabesh <prabesh.bista369@gmail.com>
2022-01-29 15:40:56 +0545

    Linear Seraching using Java Language

---
+++ src/DSA/LinearSearching.java
@@ -0,0 +1,19 @@
+package DSA;
+
+public class LinearSearching {
+    int Linearsearch(int[] list,int target){
+        int position=-1;
+        int n=0;
+        while (n<list.length){
+            if(list[n]==target){
+                position=n;
+                return position;
+            }
+
+            ++n;
+        }
+
+
+    return position;
+    }
+}
--- src/DSA/Main.java
+++ src/DSA/Main.java
@@ -6,17 +6,28 @@ public class Main {
     public static void main(String[] args) {
         // Bubble Sorting in Java
 //        BubbleSort bubbleSort=new BubbleSort();
-//        int[] list={22,1,55,3,7,10,99,34,20};
+        int[] list={22,1,55,3,7,10,99,34,20};
 //        int[] list1= bubbleSort.bubblesort(list);
 //        System.out.println(list1[list1.length-1]);
 
-        int[] list={3,3};
-        AddList addList=new AddList();
-        int[] list1= addList.add(list,6);
+//        int[] list={3,3};
+//        AddList addList=new AddList();
+//        int[] list1= addList.add(list,6);
+//
+//        for(int i=0;i<2;i++){
+//            System.out.println(list1[i]);
+//        }
 
-        for(int i=0;i<2;i++){
-            System.out.println(list1[i]);
+
+        LinearSearching linearSearching=new LinearSearching();
+        int index=linearSearching.Linearsearch(list,34);
+        if(index==-1){
+            System.out.println("There is no number present in the list.");
+
+        }else{
+            System.out.println("Your number exist at "+index);
         }
+
 
     }
 }
