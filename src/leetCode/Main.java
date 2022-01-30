bf5c7ecd31de8628b1ffc7969554b723400a5a8e
Prabesh <prabesh.bista369@gmail.com>
2022-01-30 23:49:41 +0545

    isPalindrone

--- src/leetCode/Main.java
+++ src/leetCode/Main.java
@@ -2,6 +2,9 @@ package leetCode;
 
 public class Main {
     public static void main(String[] args) {
+        Solution solution=new Solution();
+        boolean x=solution.isPalindrome(123);
+        System.out.println(x);
 
     }
 }
--- src/leetCode/Solution.java
+++ src/leetCode/Solution.java
@@ -3,8 +3,14 @@ package leetCode;
 public class Solution {
     public boolean isPalindrome(int x){
         String str=String.valueOf(x);
-
-        return  true;
+        int length=str.length();
+        char a=str.charAt(0);
+        char b=str.charAt(length-1);
+        if(a==b){
+            return  true;
+        }else{
+            return  false;
+        }
 
     }
 }
