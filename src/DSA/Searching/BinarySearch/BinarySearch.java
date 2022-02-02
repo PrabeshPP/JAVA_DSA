1e57a4c9b72d6923b0728d728cdbeb10da994a18
Prabesh <prabesh.bista369@gmail.com>
2022-02-02 19:46:41 +0545

    Binary Search

--- src/DSA/Searching/BinarySearch/BinarySearch.java
+++ src/DSA/Searching/BinarySearch/BinarySearch.java
@@ -9,18 +9,19 @@ public class BinarySearch {
     int find(int[] arr,int target){
         int initialPos=0;
         int finalPos=arr.length-1;
-        int mid=(initialPos+finalPos)/2;
+
         while(initialPos<=finalPos){
+            int mid=(initialPos+finalPos)/2;
             if(target==arr[mid]){
                 return mid;
 
             }
             else if(target>arr[mid]){
                 initialPos=mid+1;
-                mid=(initialPos+finalPos)/2;
+
             }else{
                 finalPos=mid-1;
-                mid=(initialPos+finalPos)/2;
+
 
             }
 
