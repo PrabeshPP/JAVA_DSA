4e0a2713c4893cc53096ef9605ff82e7a7839ed1
Prabesh <prabesh.bista369@gmail.com>
2022-02-02 09:31:20 +0545

    isPalindrone

---
+++ src/leetCode/Solution2.java
@@ -0,0 +1,9 @@
+package leetCode;
+
+public class Solution2 {
+    void muliolication(int n){
+        for(int i=1;i<=10;++i){
+            System.out.println(i+" * "+n+" = "+i*n);
+        }
+    }
+}
