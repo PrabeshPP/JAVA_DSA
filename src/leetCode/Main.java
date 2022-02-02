f11e499c3b9345f8f1d6587cb239cbb2eb3ad837
Prabesh <prabesh.bista369@gmail.com>
2022-02-02 09:32:16 +0545

    isPalindrone

--- src/leetCode/Main.java
+++ src/leetCode/Main.java
@@ -2,9 +2,12 @@ package leetCode;
 
 public class Main {
     public static void main(String[] args) {
-        Solution solution=new Solution();
-        boolean x=solution.isPalindrome(123);
-        System.out.println(x);
+//        Solution solution=new Solution();
+//        boolean x=solution.isPalindrome(123);
+//        System.out.println(x);
+
+        Solution2 solution2=new Solution2();
+        solution2.muliolication(8);
 
 
 
