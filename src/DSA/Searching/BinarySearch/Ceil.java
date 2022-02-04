4c217bc22a760c71497d5f06132ad6494da6ef62
Prabesh <prabesh.bista369@gmail.com>
2022-02-04 16:41:37 +0545

    Binary Search

---
+++ src/DSA/Searching/BinarySearch/Ceil.java
@@ -0,0 +1,19 @@
+package DSA.Searching.BinarySearch;
+
+public class Ceil {
+    int findCeil(int[] nums,int target){
+        int start=0;
+        int end=nums.length;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target){
+                return mid;
+            }else if(nums[mid]<target){
+                start=mid+1;
+            }else{
+                end=end-1;
+            }
+        }
+        return start;
+    }
+}
