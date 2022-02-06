809fbce59e944709f3ce84242436c7ddfd4f34a8
Prabesh <prabesh.bista369@gmail.com>
2022-02-06 19:27:33 +0545

    Binary Search,Ceil Number,Floor Number

--- src/DSA/Searching/BinarySearch/InfiniteNumbers.java
+++ src/DSA/Searching/BinarySearch/InfiniteNumbers.java
@@ -1,12 +1,36 @@
 package DSA.Searching.BinarySearch;
 
 public class InfiniteNumbers {
-    int findNumberInfinite(int[] nums,int target){
-        int start=nums[0];
-        int end=nums[4];
-        if(end>target) {
-            int mid = start + (end - start) / 2;
+
+    int findAns(int[] nums,int target){
+        //
+        int start=0;
+        int end=1;
+
+        //conditional loops
+        while(target>nums[end]){
+         int   newStart=end+1;
+         end=end+(end-start+1)*2;
+         start=newStart;
+        }
+        int index=binarySearch(nums,target,start,end);
+
+        return index;
+
+
+
+    }
+
+
+
+    int binarySearch(int[] nums,int target,int start1,int end1){
+        int len=1;
+        int start=start1;
+        int end=end1;
+
+
             while (start <= end) {
+                int mid = start + (end - start) / 2;
                 if (nums[mid] == target) {
                     return mid;
                 } else if (nums[mid] > target) {
@@ -15,8 +39,10 @@ public class InfiniteNumbers {
                     start = mid + 1;
                 }
             }
+            return  -1;
 
         }
+
     }
 
-}
+
