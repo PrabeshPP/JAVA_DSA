59d2feadf4180d14b0c110ccfe1340a7909d46dc
Prabesh <prabesh.bista369@gmail.com>
2022-02-08 20:39:27 +0545

    Mountain Array

--- src/leetCode/RotateArray.java
+++ src/leetCode/RotateArray.java
@@ -4,13 +4,14 @@ public class RotateArray {
     int search(int[] nums, int target) {
         int index=-1;
         int peakIndex=peakIndex(nums);
+        System.out.println(peakIndex);
         int index1=binarySearch(nums,target,0,peakIndex);
         int index2=binarySearch(nums,target,peakIndex+1,nums.length-1);
 
         if(index1==-1 && index2==-1){
             return  -1;
         }else{
-            if(index1>index2){
+            if(index1>0 &&index2<0){
                 return  index1;
             }else{
                 return  index2;
@@ -22,19 +23,8 @@ public class RotateArray {
 
     }
 
-    int peakIndex(int[] nums){
-        int start=0;
-        int end=nums.length-1;
-        while(start<end){
-            int mid=start+(end-start)/2;
-            if(nums[mid]>nums[mid+1]){
-                end=mid;
-            }else{
-                start=mid+1;
-            }
-        }
+    int pivotIndex(int[] nums){
 
-        return start;
     }
 
     int binarySearch(int[] nums,int target,int start,int end){
