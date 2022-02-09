ff06870a045b04faa359df985951e2de22961540
Prabesh <prabesh.bista369@gmail.com>
2022-02-09 20:42:42 +0545

    Simple Interest

---
+++ src/Assignments/two/SI.java
@@ -0,0 +1,9 @@
+package Assignments.two;
+
+public class SI {
+    double simpleInterest(double p,double t,double r){
+        double totalInterest=(p*t*r)/100;
+
+        return  totalInterest;
+    }
+}
