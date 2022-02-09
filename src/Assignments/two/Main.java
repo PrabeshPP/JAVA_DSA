10b33099f2e866307300d7a2f4ce98a2144ea28b
Prabesh <prabesh.bista369@gmail.com>
2022-02-09 20:35:50 +0545

    Number Status

--- src/Assignments/two/Main.java
+++ src/Assignments/two/Main.java
@@ -1,4 +1,20 @@
 package Assignments.two;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        NumberStatus numberStatus=new NumberStatus();
+        System.out.print("Enter the number to check whether the given number is even or odd:");
+        int number=scanner.nextInt();
+        boolean isEven= numberStatus.isEven(number);
+        if(isEven){
+            System.out.println("The number is even.");
+        }else{
+            System.out.println("The number is odd.");
+        }
+
+    }
+
 }
--- src/Assignments/two/NumberStatus.java
+++ src/Assignments/two/NumberStatus.java
@@ -1,4 +1,11 @@
 package Assignments.two;
 
 public class NumberStatus {
+    boolean isEven(int number){
+        if(number%2==0){
+            return  true;
+        }else{
+            return  false;
+        }
+    }
 }
