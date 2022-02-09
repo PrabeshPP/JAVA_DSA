7be7e5a350792f80d7dfb38e79c6623d6b7714fb
Prabesh <prabesh.bista369@gmail.com>
2022-02-09 18:40:58 +0545

    difference of sum and product

---
+++ src/Assignments/one/Main.java
@@ -0,0 +1,11 @@
+package Assignments.one;
+
+public class Main {
+    public static void main(String[] args) {
+        HCFLCM hcflcm=new HCFLCM();
+        int value=hcflcm.findHCF(20,40);
+        System.out.println(value);
+    }
+
+
+}
