ab44c2bf495c0980b95ac462ceb84786d1c494b5
Prabesh <prabesh.bista369@gmail.com>
2022-02-09 20:38:17 +0545

    Greetings

---
+++ src/Assignments/two/Greetings.java
@@ -0,0 +1,9 @@
+package Assignments.two;
+
+import java.sql.Time;
+
+public class Greetings {
+    void greet(String name){
+        System.out.println("Hello"+name+",welcome to the programming world.");
+    }
+}
