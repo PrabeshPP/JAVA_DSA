3692c04b6192505c641f8a2d61478439fb7f569b
Prabesh <prabesh.bista369@gmail.com>
2022-02-10 21:48:15 +0545

    Searching index of pivot number in Rotated array

---
+++ src/leetCode/SplitArray.java
@@ -0,0 +1,39 @@
+package leetCode;
+
+import java.util.Arrays;
+
+public class SplitArray {
+
+    int findMaxSumofSplitArray(int[] arr,int m){
+        int start=0;
+        int end=0;
+        for(int i=0;i<arr.length;++i){
+            start= Math.max(start,arr[i]);
+            end +=arr[i];
+        }
+
+        while(start<end){
+            int mid=start+(end-start)/2;
+            int sum=0;
+            int arrayNumber=0;
+            for(int num:arr){
+                if(sum+num>mid){
+                    sum=num;
+                    arrayNumber++;
+                }else{
+                    sum+=num;
+                }
+            }
+
+            if(arrayNumber>m){
+                start=mid;
+            }else{
+                end=mid;
+            }
+
+
+        }
+
+        return start;
+    }
+}
