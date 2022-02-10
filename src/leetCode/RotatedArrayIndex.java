bb10a815a2c798f124db8aded54f11ea49067c62
Prabesh <prabesh.bista369@gmail.com>
2022-02-10 20:32:07 +0545

    Simple Interest

--- src/leetCode/RotatedArrayIndex.java
+++ src/leetCode/RotatedArrayIndex.java
@@ -8,10 +8,10 @@ public class RotatedArrayIndex {
         while(start<=end){
             int mid=start+(end-start)/2;
             if(mid<end && nums[mid]>nums[mid+1]){
-                return  mid;
+                return  mid+1;
             }
             if( mid>start && nums[mid]<nums[mid-1] ){
-                return mid-1;
+                return mid;
             }
 
             if(nums[mid]<=nums[start]){
