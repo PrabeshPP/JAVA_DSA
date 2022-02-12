a96e550b1df572d6abc107c3c9ba70e275125120
Prabesh <prabesh.bista369@gmail.com>
2022-02-12 10:06:20 +0545

    How Many Numbers Are Smaller Than the Current Number

--- src/leetCode/SmallArray.java
+++ src/leetCode/SmallArray.java
@@ -4,6 +4,18 @@ public class SmallArray {
     public int[] smallerNumbersThanCurrent(int[] nums) {
         int[] result=new int[nums.length];
 
+        for(int i=0;i<nums.length;i++){
+            int num1=nums[i];
+            int sm=0;
+            for(int j=i+1;j<nums.length;j++){
+                if(num1>nums[j]){
+                    sm++;
+                }
+
+            }
+            result[i]=sm;
+        }
+
 
 
         return  result;
