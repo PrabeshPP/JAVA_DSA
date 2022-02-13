b65fad4e0e48b7820061f02696972c31a771812e
Prabesh <prabesh.bista369@gmail.com>
2022-02-13 21:00:17 +0545

    Array in Java

--- src/Array/Main.java
+++ src/Array/Main.java
@@ -7,10 +7,15 @@ public class Main {
         // Array declaration
 //        int[] arr;
         // Array initialization , the object is store in the heap(memory)
+        //Array of Primitives
        int[] arr={1,2,3,4,5};
 
 
         // It will convert array to string
         System.out.print(Arrays.toString(arr));
+
+        // Array of an Objects;
+
+
     }
 }
