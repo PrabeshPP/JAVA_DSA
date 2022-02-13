75cfe1c5cf3929b01cc701ebf131b9e08a65e09e
Prabesh <prabesh.bista369@gmail.com>
2022-02-13 20:57:22 +0545

    Array in Java

--- src/Array/Main.java
+++ src/Array/Main.java
@@ -3,9 +3,10 @@ package Array;
 public class Main {
     public static void main(String[] args) {
         // Array declaration
-        int[] arr;
+//        int[] arr;
         // Array initialization , the object is store in the heap(memory)
-        arr=new int[5];
+       int[] arr={1,2,3,4,5};
+
         System.out.print(arr[3]);
     }
 }
