2ed85a7de640a60a1031041b1666792b997d0e8d
Prabesh <prabesh.bista369@gmail.com>
2022-02-13 21:03:41 +0545

    Array in Java

--- src/Array/Main.java
+++ src/Array/Main.java
@@ -1,20 +1,32 @@
 package Array;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scanner=new Scanner(System.in);
+
         // Array declaration
 //        int[] arr;
         // Array initialization , the object is store in the heap(memory)
         //Array of Primitives
-       int[] arr={1,2,3,4,5};
+//       int[] arr={1,2,3,4,5};
 
 
         // It will convert array to string
-        System.out.print(Arrays.toString(arr));
+//        System.out.print(Arrays.toString(arr));
 
         // Array of an Objects;
+
+        String[] str=new String[4];
+
+        for(int i=0;i<str.length;++i){
+            str[i]=scanner.next();
+        }
+
+        System.out.println(Arrays.toString(str));
 
 
     }
