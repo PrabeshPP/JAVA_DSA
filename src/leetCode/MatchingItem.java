c3e9c4badf2e1b644b17720a766d779f98043d07
Prabesh <prabesh.bista369@gmail.com>
2022-02-13 21:33:15 +0545

    Matching Item

--- src/leetCode/MatchingItem.java
+++ src/leetCode/MatchingItem.java
@@ -1,4 +1,15 @@
 package leetCode;
 
+import java.util.List;
+
 public class MatchingItem {
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+
+        int matchNo=0;
+
+        for(int i=0;i<items.size();++i){
+
+        }
+    return matchNo;
+    }
 }
