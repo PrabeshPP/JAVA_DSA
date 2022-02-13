166f53a062a764238566db0d606a999243c7f3c8
Prabesh <prabesh.bista369@gmail.com>
2022-02-13 10:23:20 +0545

    target Array

--- src/leetCode/TargetArray.java
+++ src/leetCode/TargetArray.java
@@ -1,12 +1,12 @@
 package leetCode;
 
+import java.awt.*;
+
 public class TargetArray {
     public int[] createTargetArray(int[] nums, int[] index) {
         int[] target=new int[nums.length];
         for(int i=0;i<nums.length;i++){
-//            System.out.print(index[i]+",");
-            System.out.print(nums[i]+",");
-            target[i]=nums[index[i]];
+//
         }
 
 
