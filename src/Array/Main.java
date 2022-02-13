b0ce99238054f558a41273d3cb438fce3eca91e5
Prabesh <prabesh.bista369@gmail.com>
2022-02-13 20:38:40 +0545

    Array in Java

---
+++ src/Array/Main.java
@@ -0,0 +1,11 @@
+package Array;
+
+public class Main {
+    public static void main(String[] args) {
+        // Array declaration
+        int[] arr;
+        // Array initialization , the object is store in the heap(memory)
+        arr=new int[5];
+        System.out.print(arr[3]);
+    }
+}
