481abc00b406f3fe7236a3956d8b9ba259626819
Prabesh <prabesh.bista369@gmail.com>
2022-02-14 22:32:29 +0545

    Highest Altitude

--- src/leetCode/HighestAltitude.java
+++ src/leetCode/HighestAltitude.java
@@ -6,17 +6,18 @@ public class HighestAltitude {
     public int largestAltitude(int[] gain) {
         int[] arr=new int[gain.length+1];
         arr[0]=0;
-        arr[1]=gain[1];
+        arr[1]=gain[0];
         System.out.println(gain[1]);
         int b=0;
-        for(int i=2;i<=gain.length;i++){
-            if(i!=gain.length){
-                 b=gain[i]-gain[i-1];
+
+        for(int i=2;i<arr.length;i++){
+            if(i<= gain.length){
+
             }
 
-            arr[i]=b;
         }
-            System.out.print(Arrays.toString(arr));
+
+
         int highest=gain[0];
         for(int i=1;i<arr.length;i++){
             if(arr[i]>highest){
