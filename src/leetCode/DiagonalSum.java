f20e905614d99ec59c8bebba3e5d0a1d23876787
Prabesh <prabesh.bista369@gmail.com>
2022-02-16 16:58:51 +0545

    DiagonalSum

--- src/leetCode/DiagonalSum.java
+++ src/leetCode/DiagonalSum.java
@@ -4,6 +4,30 @@ public class DiagonalSum {
     public int diagonalSum(int[][] mat) {
         int sum=0;
         //First diagonal Sum
+        for(int i=0;i<mat.length;i++){
+            int value=mat[i][i];
+            sum+=value;
+        }
+
+        if(mat.length %2==0){
+            for(int i=0;i<mat.length;i++){
+                int value=mat[mat.length-1-i][mat.length-1-i];
+                sum+=value;
+            }
+
+        }else{
+            for(int i=0;i<mat.length;i++){
+                if(i== mat.length/2){
+                    continue;
+
+                }
+                int value=mat[mat.length-1-i][mat.length-1-i];
+                sum+=value;
+
+            }
+        }
+
+
 
         return sum;
 
