0f04f3990131782904f81c3931cec7ae2c863f6f
Prabesh <prabesh.bista369@gmail.com>
2022-02-16 18:57:20 +0545

    TransposeMatrix

--- src/leetCode/TransposeMatrix.java
+++ src/leetCode/TransposeMatrix.java
@@ -2,7 +2,7 @@ package leetCode;
 
 public class TransposeMatrix {
     public int[][] transpose(int[][] matrix) {
-        int[][] newMatrix=new int[matrix.length][matrix[0].length];
+        int[][] newMatrix=new int[matrix[0].length][matrix.length];
 
 
 
