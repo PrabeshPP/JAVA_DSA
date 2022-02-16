64d59eabd557812f6e0667e671ffe2f7690ec4cd
Prabesh <prabesh.bista369@gmail.com>
2022-02-16 18:44:10 +0545

    EvenNumberDigits

--- src/leetCode/EvenNumberDigits.java
+++ src/leetCode/EvenNumberDigits.java
@@ -2,6 +2,18 @@ package leetCode;
 
 public class EvenNumberDigits {
     public int findNumbers(int[] nums) {
+        int counter=0;
+
+        for(int i:nums){
+            String strNumber=String.valueOf(i);
+            int length=strNumber.length();
+            if(length%2==0){
+                counter++;
+            }
+        }
+
+
+        return  counter;
 
     }
 }
