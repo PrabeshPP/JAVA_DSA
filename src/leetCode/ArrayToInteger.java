61b0d47f1cdb0c269daabe9f93c060043c1d09bb
Prabesh <prabesh.bista369@gmail.com>
2022-02-16 19:21:13 +0545

    ArrayInteger

---
+++ src/leetCode/ArrayToInteger.java
@@ -0,0 +1,21 @@
+package leetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayToInteger {
+    public List<Integer> addToArrayForm(int[] num, int k) {
+        List<Integer> list=new ArrayList<Integer>();
+        String stno="";
+        for(int i=0;i<num.length;i++){
+           stno+=String.valueOf(num[i]);
+        }
+
+
+
+
+
+        return list;
+
+    }
+}
