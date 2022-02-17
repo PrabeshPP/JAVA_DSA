9328d96b5590408502b66aad74846593140fb665
Prabesh <prabesh.bista369@gmail.com>
2022-02-17 21:51:03 +0545

    Array Rotation

--- src/leetCode/FindRotation.java
+++ src/leetCode/FindRotation.java
@@ -7,17 +7,13 @@ public class FindRotation {
         boolean result=false;
         int[][] newArr=new int[mat.length][mat.length];
 
+        int maxRotate=mat.length;
+
         for(int i=0;i<mat.length;i++){
                 for(int j=0;j<mat.length;j++){
                     newArr[j][mat.length-1-i]=mat[i][j];
                 }
             }
-
-        int[][] newArr1=new int[mat.length][mat.length];
-
-        for(int[] item:newArr){
-            System.out.println(Arrays.toString(item));
-        }
 
 
 
