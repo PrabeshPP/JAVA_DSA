6c04f7d831bea9863a480ef8671ffad8d7ea90dc
Prabesh <prabesh.bista369@gmail.com>
2022-02-17 17:44:28 +0545

    Max. Population Year

--- src/leetCode/MaxPopYear.java
+++ src/leetCode/MaxPopYear.java
@@ -3,6 +3,11 @@ package leetCode;
 public class MaxPopYear {
     public int maximumPopulation(int[][] logs) {
         int year=0;
+        for(int i=0;i<logs.length;i++){
+            int birth=logs[i][0];
+            int death=logs[i][1];
+
+        }
 
 
 
