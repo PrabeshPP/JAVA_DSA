d2de661fcadcee148270de6ce64ab21ced04caf1
Prabesh <prabesh.bista369@gmail.com>
2022-02-20 20:18:39 +0545

    LuckyNumber

--- src/leetCode/LuckyNumber.java
+++ src/leetCode/LuckyNumber.java
@@ -6,6 +6,25 @@ import java.util.List;
 public class LuckyNumber {
     public List<Integer> luckyNumbers (int[][] matrix) {
         List<Integer> li=new ArrayList<Integer>();
+        int luckyNumber=0;
+        int minRow=matrix[0][0];
+        int column=0;
+        for(int i=1;i<matrix.length;i++){
+            for(int j=1;j<matrix.length;j++){
+                if(minRow>matrix[i][j]){
+                    minRow=matrix[i][j];
+                    column=j;
+                }
+
+            }
+            for(int k=0;k< matrix.length;k++){
+                if(minRow>matrix[k][column]){
+                    luckyNumber=minRow;
+                }
+            }
+        }
+
+        System.out.print(luckyNumber);
 
         return li;
     }
