eed33b387aee6eaabe359b8085e7939a701433a5
Prabesh <prabesh.bista369@gmail.com>
2022-02-20 18:36:53 +0545

    LuckyNumber

---
+++ src/leetCode/LuckyNumber.java
@@ -0,0 +1,12 @@
+package leetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LuckyNumber {
+    public List<Integer> luckyNumbers (int[][] matrix) {
+        List<Integer> li=new ArrayList<Integer>();
+
+        return li;
+    }
+}
