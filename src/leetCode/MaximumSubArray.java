c0c6f5ab193a27bb9e39b526b705694c106bb049
Prabesh <prabesh.bista369@gmail.com>
2022-02-20 22:00:00 +0545

    MaximumSubArray

--- src/leetCode/MaximumSubArray.java
+++ src/leetCode/MaximumSubArray.java
@@ -2,10 +2,14 @@ package leetCode;
 
 public class MaximumSubArray {
     public int maxSubArray(int[] nums) {
+        //Kadane's Algorithm should be used
+
         int maxSum=0;
         for(int i=0;i<nums.length;i++){
             maxSum+=nums[i];
         }
+
+
 
         return  maxSum;
     }
