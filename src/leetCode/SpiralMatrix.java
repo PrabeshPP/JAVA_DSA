18052fbcf2270e57403b772ac70a17dd7ee266b6
Prabesh <prabesh.bista369@gmail.com>
2022-02-22 21:13:50 +0545

    SpiralMatrix

---
+++ src/leetCode/SpiralMatrix.java
@@ -0,0 +1,21 @@
+package leetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SpiralMatrix {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> list=new ArrayList<Integer>();
+        int i=0;
+        int totalElem= matrix.length*matrix[i].length;
+        int row=0;
+        int col=0;
+        while(i<totalElem){
+
+
+
+        }
+
+        return  list;
+    }
+}
