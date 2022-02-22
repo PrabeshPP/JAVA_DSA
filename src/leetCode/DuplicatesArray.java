703a63689a3c394431ffea270aa113ee1a2e3e7e
Prabesh <prabesh.bista369@gmail.com>
2022-02-22 17:33:45 +0545

    Duplicates Array

--- src/leetCode/DuplicatesArray.java
+++ src/leetCode/DuplicatesArray.java
@@ -1,4 +1,22 @@
 package leetCode;
 
 public class DuplicatesArray {
+    public int removeDuplicates(int[] nums) {
+        int len=0;
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]==nums[j]){
+                    nums[j]=-101;
+                }
+
+            }
+        }
+
+        for(int i=0;i<nums.length;i++){
+            if(i!=-101){
+                len++;
+            }
+        }
+    return len;
+    }
 }
