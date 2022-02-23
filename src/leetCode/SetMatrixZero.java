ae9e4a6b61454a24d399adc2812ab4b1cfe9c7e2
Prabesh <prabesh.bista369@gmail.com>
2022-02-23 20:32:20 +0545

    Set Matrix Zeros

--- src/leetCode/SetMatrixZero.java
+++ src/leetCode/SetMatrixZero.java
@@ -1,12 +1,17 @@
 package leetCode;
 
+import java.util.ArrayList;
+
 public class SetMatrixZero {
     public void setZeroes(int[][] matrix) {
+        ArrayList<ArrayList<Integer>> list=new ArrayList<ArrayList<Integer>>();
         for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[i].length;j++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(matrix[i][j]==0){
+                    list.add();
 
+                }
             }
         }
-
     }
 }
