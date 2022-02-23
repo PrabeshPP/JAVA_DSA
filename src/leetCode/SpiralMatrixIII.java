2a45c9ba59e67b9bfa09fab9ab13b36433aaeeec
Prabesh <prabesh.bista369@gmail.com>
2022-02-23 19:36:48 +0545

    SpiralMatrix III Storing the Co-ordinates.

--- src/leetCode/SpiralMatrixIII.java
+++ src/leetCode/SpiralMatrixIII.java
@@ -3,7 +3,8 @@ package leetCode;
 public class SpiralMatrixIII {
     public int[][] spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
         int[][] matrix=new int[rows][cols];
-        int top=0,down=
+       int n=1; // It is the sequence of the index;
+
 
         return  matrix;
     }
