64c0f28a6b62301eb32b34f17759906d110e8805
Prabesh <prabesh.bista369@gmail.com>
2022-02-25 17:12:31 +0545

    Product of Array Except Self

--- src/leetCode/ProductOfArray.java
+++ src/leetCode/ProductOfArray.java
@@ -2,6 +2,19 @@ package leetCode;
 
 public class ProductOfArray {
     public int[] productExceptSelf(int[] nums) {
+        int[] newArr=new int[nums.length];
+        for(int i=0;i< nums.length;i++){
+            int product=1;
+            for(int j=0;j<nums.length;j++){
+                if(i==j){
+                    continue;
+                }
+                product=product*nums[j];
 
+
+            }
+            newArr[i]=product;
+        }
+        return newArr;
     }
 }
