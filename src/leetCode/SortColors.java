5eba12e3994bb23cac59e2f01b698d9c3190c1f4
Prabesh <prabesh.bista369@gmail.com>
2022-02-27 21:35:05 +0545

    Sorting Colors in Arrays

--- src/leetCode/SortColors.java
+++ src/leetCode/SortColors.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 
 public class SortColors {
     public void sortColors(int[] nums) {
+        //I have used Bubble Sorting which can be lengthy to sort the array
+        //Will come up with different solution after learning sorting tutorial
         for(int i=0;i<nums.length;i++){
             for(int j=0;j<nums.length-1;j++){
                 if(nums[j]>nums[j+1]){
