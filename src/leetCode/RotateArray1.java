31d758aa504e609aeb342072d75345d2f7a8bee0
Prabesh <prabesh.bista369@gmail.com>
2022-02-27 21:11:00 +0545

    Rotate the Array(Medium)(with O(n) soln)

--- src/leetCode/RotateArray1.java
+++ src/leetCode/RotateArray1.java
@@ -3,23 +3,51 @@ package leetCode;
 import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class RotateArray1 {
     public void rotate(int[] nums, int k) {
         //This solution would work but exceeds time limit
-        ArrayList<Integer> list=new ArrayList<Integer>();
-        for(int i=0;i<nums.length;i++){
-            list.add(nums[i] );
-        }
+//        ArrayList<Integer> list=new ArrayList<Integer>();
+//        for(int i=0;i<nums.length;i++){
+//            list.add(nums[i] );
+//        }
+//
+//    k=k%nums.length;
+//        for(int i=0;i<k;i++){
+//            list.add(0,list.get(nums.length-1));
+//        }
+//        for(int i=0;i< nums.length;i++){
+//            nums[i]=list.get(i);
+//        }
 
-    k=k%nums.length;
-        for(int i=0;i<k;i++){
-            list.add(0,list.get(nums.length-1));
-        }
-        for(int i=0;i< nums.length;i++){
-            nums[i]=list.get(i);
-        }
 
-        System.out.println(Arrays.toString(nums));
+        //Soln 2,split the array into two arrays;
+
+
+        //reverse Part1
+        reverse(nums,0, nums.length-1-k);
+        //reverse Part2
+        reverse(nums, nums.length-1-k, nums.length-1);
+
+        //reverse  All
+        reverse(nums,0, nums.length-1);
+
+
+
+    }
+
+    public void reverse(int[] num,int length1,int length2){
+        int li=length1;
+        int ri=length2;
+
+        while(li<ri){
+            int temp=num[li];
+             num[li]=num[ri];
+             num[ri]=temp;
+             li++;
+             ri--;
+
+        }
     }
 }
