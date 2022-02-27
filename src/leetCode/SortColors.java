d23b89e386a3159594ce6095e847e69b6e89758c
Prabesh <prabesh.bista369@gmail.com>
2022-02-27 21:32:42 +0545

    Sorting Colors in Arrays

---
+++ src/leetCode/SortColors.java
@@ -0,0 +1,20 @@
+package leetCode;
+
+import java.util.Arrays;
+
+public class SortColors {
+    public void sortColors(int[] nums) {
+        for(int i=0;i<nums.length;i++){
+            for(int j=0;j<nums.length-1;j++){
+                if(nums[j]>nums[j+1]){
+                    int temp=nums[j];
+                    nums[j]=nums[j+1];
+                    nums[j+1]=temp;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(nums));
+
+    }
+}
