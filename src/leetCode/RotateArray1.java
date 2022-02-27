9abcdd430d0f880b2ded484ea9e260653a633c23
Prabesh <prabesh.bista369@gmail.com>
2022-02-27 19:18:49 +0545

    Rotate the Array(Medium)

--- src/leetCode/RotateArray1.java
+++ src/leetCode/RotateArray1.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 public class RotateArray1 {
     public void rotate(int[] nums, int k) {
        int[] newArr=new int[nums.length];
-       for(int i=0;i<k;k++){
+       for(int i=0;i<k;i++){
            newArr[k-1-i]=nums[nums.length-1-i];
        }
        System.out.println(Arrays.toString(newArr));
