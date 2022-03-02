d9e78a4bde3a0760e5ac7dd023f89ab426a4b53c
Prabesh <prabesh.bista369@gmail.com>
2022-03-02 18:16:17 +0545

    BinarySearch in 2D Array

--- src/DSA/Searching/BinarySearch/BinarySearch2DArray.java
+++ src/DSA/Searching/BinarySearch/BinarySearch2DArray.java
@@ -1,4 +1,13 @@
 package DSA.Searching.BinarySearch;
 
 public class BinarySearch2DArray {
+    int[] binarySearch2DArray(int[][] arr,int target){
+        int[] pos=new int[2];
+        int lowerBound=0;
+        int upperBound=arr[0].length;
+
+
+        return pos;
+
+    }
 }
