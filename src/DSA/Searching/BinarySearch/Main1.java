f379b71121428d86408d35d9346558445899509c
Prabesh <prabesh.bista369@gmail.com>
2022-03-02 18:13:43 +0545

    BinarySearch in 2D Array

--- src/DSA/Searching/BinarySearch/Main1.java
+++ src/DSA/Searching/BinarySearch/Main1.java
@@ -2,6 +2,13 @@ package DSA.Searching.BinarySearch;
 
 public class Main1 {
     public static void main(String[] args) {
+        int[][] arr={
+                {10,20,30,40},
+                {15,25,35,45},
+                {28,29,37,49},
+                {33,34,38,50}
+        };
+
 
     }
 }
