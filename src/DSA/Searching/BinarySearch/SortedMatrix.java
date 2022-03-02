6103b6958d4b458783ff407a65a0e12b755e407c
Prabesh <prabesh.bista369@gmail.com>
2022-03-02 19:04:36 +0545

    Sorted Matrix in Java

--- src/DSA/Searching/BinarySearch/SortedMatrix.java
+++ src/DSA/Searching/BinarySearch/SortedMatrix.java
@@ -15,10 +15,33 @@ public class SortedMatrix {
 
     }
 
+    static  int[] search(int[][] matrix,int row,int colStart,int colEnd,int target){
+        while(colStart<=colEnd){
+            int mid=colStart+(colEnd-colStart)/2;
+            if(matrix[row][mid]==target){
+                return  new int[]{row,mid};
+            }else if(matrix[row][mid]<target){
+                colStart=mid+1;
+
+            }else{
+                colEnd=mid-1;
+            }
+        }
+
+        return  new int[]{-1,-1};
+    };
+
     static  int[] searchMatrix(int[][] matrix,int target){
+        int[] pos=new int[2];
         int rows= matrix.length;
         int column=matrix[0].length;
+        if(rows==1){
+           pos= search(matrix,0,0,2,target);
+           return pos;
+        }else{
 
+        }
+        return new int[] {-1,-1};
     }
 
 }
