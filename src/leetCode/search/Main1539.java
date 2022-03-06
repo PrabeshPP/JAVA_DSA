68ffa1fca8b22fd6eb9a435569dda8c7314b3671
Prabesh <prabesh.bista369@gmail.com>
2022-03-06 20:49:20 +0545

    35. Search Insert Position

--- src/leetCode/search/Main1539.java
+++ src/leetCode/search/Main1539.java
@@ -2,9 +2,13 @@ package leetCode.search;
 
 public class Main1539 {
     public static void main(String[] args) {
-        int[] arr={2,3,4,7,9,11,14,15};
-        MissingInteger missingInteger=new MissingInteger();
-        int missingInteger1=missingInteger.findKthPositive(arr,5);
-        System.out.println(missingInteger1);
+        int[] arr={1,3,5,6};
+//        MissingInteger missingInteger=new MissingInteger();
+//        int missingInteger1=missingInteger.findKthPositive(arr,5);
+//        System.out.println(missingInteger1);
+
+        SearchInsertPosition searchInsertPosition=new SearchInsertPosition();
+        int pos=searchInsertPosition.searchInsert(arr,7);
+        System.out.println(pos);
     }
 }
--- src/leetCode/search/SearchInsertPosition.java
+++ src/leetCode/search/SearchInsertPosition.java
@@ -3,21 +3,29 @@ package leetCode.search;
 public class SearchInsertPosition {
     public int searchInsert(int[] nums, int target) {
         int index=0;
+        int s=0;
         if(nums[0]>target){
             return 0;
         }else if(nums[nums.length-1]<target){
             return nums.length;
         }else{
-            int s=0;
+
             int e=nums.length-1;
             while(s<=e){
                 int m=s+(e-s)/2;
+                if(nums[m]==target){
+                    return  m;
+                }else if(target>nums[m]){
+                    s=m+1;
+                }else{
+                    e=m-1;
+                }
 
             }
         }
 
 
-
+       index=s;
         return index;
 
     }
