4fdd576ec809d3aa38a6825d0b8b7fe49ca62208
Prabesh <prabesh.bista369@gmail.com>
2022-03-06 21:06:57 +0545

    1351. Count Negative Numbers in a Sorted Matrix

--- src/leetCode/search/CountNegative.java
+++ src/leetCode/search/CountNegative.java
@@ -3,6 +3,8 @@ package leetCode.search;
 public class CountNegative {
     public int countNegatives(int[][] grid) {
         int number=0;
+        int lower_bound=0;
+
 
 
         return number;
--- src/leetCode/search/Main1539.java
+++ src/leetCode/search/Main1539.java
@@ -2,13 +2,23 @@ package leetCode.search;
 
 public class Main1539 {
     public static void main(String[] args) {
-        int[] arr={1,3,5,6};
-//        MissingInteger missingInteger=new MissingInteger();
-//        int missingInteger1=missingInteger.findKthPositive(arr,5);
-//        System.out.println(missingInteger1);
+//        int[] arr={1,3,5,6};
+////        MissingInteger missingInteger=new MissingInteger();
+////        int missingInteger1=missingInteger.findKthPositive(arr,5);
+////        System.out.println(missingInteger1);
+//
+//        SearchInsertPosition searchInsertPosition=new SearchInsertPosition();
+//        int pos=searchInsertPosition.searchInsert(arr,7);
+//        System.out.println(pos);
 
-        SearchInsertPosition searchInsertPosition=new SearchInsertPosition();
-        int pos=searchInsertPosition.searchInsert(arr,7);
-        System.out.println(pos);
+
+        int [][] matrix={
+                {4,3,2,-1},
+                {3,2,1,-1},
+                {1,1,-1,-2},
+                {-1,-1,-2,-3}
+        };
+
+
     }
 }
