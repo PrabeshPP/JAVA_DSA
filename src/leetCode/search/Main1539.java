830fe5f68bb2ee74edb7ad6981583ec25aeb4935
Prabesh <prabesh.bista369@gmail.com>
2022-03-06 19:21:09 +0545

    Kth Missing Positive Number

--- src/leetCode/search/Main1539.java
+++ src/leetCode/search/Main1539.java
@@ -2,6 +2,9 @@ package leetCode.search;
 
 public class Main1539 {
     public static void main(String[] args) {
-
+        int[] arr={2,3,4,7,9,11,14,15};
+        MissingInteger missingInteger=new MissingInteger();
+        int missingInteger1=missingInteger.findKthPositive(arr,5);
+        System.out.println(missingInteger1);
     }
 }
--- src/leetCode/search/MissingInteger.java
+++ src/leetCode/search/MissingInteger.java
@@ -1,4 +1,25 @@
 package leetCode.search;
 
+import java.util.ArrayList;
+
 public class MissingInteger {
+    public int findKthPositive(int[] arr, int k) {
+        int rem=arr[arr.length-1]-arr.length;
+        if(rem==0){
+            return arr[arr.length-1]+k;
+        }else{
+            int start=0;
+            int end=arr.length;
+            while(start<end){
+                int mid=start+(end-start)/2;
+               if(arr[mid]-1-mid<k){
+                   start=mid+1;
+               }else{
+                   end=mid;
+               }
+            }
+            return end+k;
+        }
+
+    }
 }
