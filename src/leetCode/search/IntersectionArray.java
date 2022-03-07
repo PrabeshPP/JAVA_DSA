a6642134f265ccb9dc7c3e00c6f236787322539c
Prabesh <prabesh.bista369@gmail.com>
2022-03-07 21:14:24 +0545

    349. Intersection of Two Arrays

--- src/leetCode/search/IntersectionArray.java
+++ src/leetCode/search/IntersectionArray.java
@@ -1,4 +1,42 @@
 package leetCode.search;
 
+import java.util.*;
+
 public class IntersectionArray {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> setList= new HashSet<Integer>();
+       Arrays.sort(nums2);
+       for(Integer number:nums1){
+           int num=binarySearch(nums2,number);
+           if(num!=-1){
+               setList.add(number);
+           }
+
+       }
+
+       int[] arr=new int[setList.size()];
+       int x=0;
+       for(Integer num:setList){
+           arr[x]=num;
+           x++;
+       }
+
+    return arr;
+    }
+
+    public int binarySearch(int[] num,int target){
+        int start=0;
+        int end=num.length;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(num[mid]==target){
+                return mid;
+            }else if(num[mid]<target){
+                start=mid+1;
+            }else{
+                end=mid-1;
+            }
+        }
+        return  -1;
+    }
 }
--- src/leetCode/search/Main1539.java
+++ src/leetCode/search/Main1539.java
@@ -1,5 +1,7 @@
 package leetCode.search;
 
+import java.util.Arrays;
+
 public class Main1539 {
     public static void main(String[] args) {
 //        int[] arr={1,3,5,6};
@@ -12,16 +14,21 @@ public class Main1539 {
 //        System.out.println(pos);
 
 
-        int [][] matrix={
-                {3,2},
-                {-3,-3},
-                {-3,-3},
-                {-3,-3}
-        };
+//        int [][] matrix={
+//                {3,2},
+//                {-3,-3},
+//                {-3,-3},
+//                {-3,-3}
+//        };
 
-        CountNegative countNegative=new CountNegative();
-        int counter=countNegative.countNegatives(matrix);
-        System.out.println(counter);
+//        CountNegative countNegative=new CountNegative();
+//        int counter=countNegative.countNegatives(matrix);
+//        System.out.println(counter);
+        int[] arr1={1,2,2,1};
+        int[] arr2={2,2};
+        IntersectionArray intersectionArray=new IntersectionArray();
+       int[] arr= intersectionArray.intersection(arr1,arr2);
+       System.out.println(Arrays.toString(arr));
 
 
     }
