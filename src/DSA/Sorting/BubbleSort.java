94679de1947c9790e0d35e56e467db15edb2d94d
Prabesh <prabesh.bista369@gmail.com>
2022-03-08 21:10:16 +0545

    Bubble Sorting Algorithm

--- src/DSA/Sorting/BubbleSort.java
+++ src/DSA/Sorting/BubbleSort.java
@@ -1,12 +1,12 @@
 package DSA.Sorting;
 
-//It takes time complexity of O(1)
+//It takes time complexity of O(N) in best case and in the worst case O(N2)
 public class BubbleSort {
     int[] bubbleSort(int[] arr){
         int len=arr.length;
 
         for(int i=0;i< arr.length-1;i++){
-            for(int j=0;j<arr.length-1;j++){
+            for(int j=0;j<arr.length-i-1;j++){
                 if(arr[j]>arr[j+1]){
                     int temp=arr[j];
                     arr[j]=arr[j+1];;
