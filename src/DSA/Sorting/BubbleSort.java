86061d2c829aa9ac4c3064232fb7a8479f2db198
Prabesh <prabesh.bista369@gmail.com>
2022-03-08 20:48:16 +0545

    Bubble Sorting Algorithm

--- src/DSA/Sorting/BubbleSort.java
+++ src/DSA/Sorting/BubbleSort.java
@@ -1,4 +1,20 @@
 package DSA.Sorting;
 
 public class BubbleSort {
+    int[] bubbleSort(int[] arr){
+        int len=arr.length;
+
+        for(int i=0;i< arr.length-1;i++){
+            for(int j=0;j<arr.length-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];;
+                    arr[j+1]=temp;
+
+                }
+            }
+        }
+
+        return arr;
+    }
 }
--- src/DSA/Sorting/Main.java
+++ src/DSA/Sorting/Main.java
@@ -1,4 +1,16 @@
 package DSA.Sorting;
 
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) {
+        int[] arr={22,5,1,18,10,99,60};
+        BubbleSort bubbleSort=new BubbleSort();
+        int[] newArr=bubbleSort.bubbleSort(arr);
+        System.out.println(Arrays.toString(newArr));
+
+    }
+
+
+
 }
