80a1e82b77540fcfe5a1f667f0c4455fd9ba453e
Prabesh <prabesh.bista369@gmail.com>
2022-03-09 20:30:38 +0545

    Selection Sort Algorithm

--- src/DSA/Sorting/Main.java
+++ src/DSA/Sorting/Main.java
@@ -1,14 +1,18 @@
 package DSA.Sorting;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         int[] arr={22,5,1,18,10,99,60};
-        BubbleSort bubbleSort=new BubbleSort();
-        int[] newArr=bubbleSort.bubbleSort(arr);
-        System.out.println(Arrays.toString(newArr));
+//        BubbleSort bubbleSort=new BubbleSort();
+//        int[] newArr=bubbleSort.bubbleSort(arr);
+//        System.out.println(Arrays.toString(newArr));
 
+        SelectionSort selectionSort=new SelectionSort();
+        int[] resultArr=selectionSort.selectionSort(arr);
+        System.out.println(Arrays.toString(resultArr));
     }
 
 
---
+++ src/DSA/Sorting/SelectionSort.java
@@ -0,0 +1,29 @@
+package DSA.Sorting;
+
+public class SelectionSort {
+    int[] selectionSort(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            //find the maximum number with the index and swap it
+            int lastIndex=arr.length-i-1;
+            int maxIndex=getMaxIndex(arr,lastIndex);
+            int temp=arr[maxIndex];
+             arr[maxIndex]=arr[lastIndex];
+             arr[lastIndex]=temp;
+
+        }
+
+        return arr;
+    }
+
+    int getMaxIndex(int[] arr,int lastIndex){
+        int maxIndex=0;
+        for(int i=0;i<=lastIndex;i++){
+            if(arr[maxIndex]<arr[i]){
+                maxIndex=i;
+            }
+        }
+        return  maxIndex;
+    }
+
+
+}
