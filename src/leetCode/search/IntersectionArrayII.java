4a0ae27b6c3df8bbfb3a9b26585cf9e23a5baa7a
Prabesh <prabesh.bista369@gmail.com>
2022-03-10 20:18:18 +0545

    350. Intersection of Two Arrays II

--- src/leetCode/search/IntersectionArrayII.java
+++ src/leetCode/search/IntersectionArrayII.java
@@ -1,7 +1,16 @@
 package leetCode.search;
 
+import java.util.Arrays;
+
 public class IntersectionArrayII {
     public int[] intersection(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+
+    }
+
+    int binarySearch(int[] arr,int start,int end,int target){
 
     }
 }
