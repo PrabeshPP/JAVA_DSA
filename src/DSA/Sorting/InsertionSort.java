35f0d6443084940941abc9dadac1194adb2c3fd9
Prabesh <prabesh.bista369@gmail.com>
2022-03-14 18:22:16 +0545

    InsertionSort Algorithm.

--- src/DSA/Sorting/InsertionSort.java
+++ src/DSA/Sorting/InsertionSort.java
@@ -5,9 +5,9 @@ public class InsertionSort {
         for(int i=0;i<arr.length-1;i++){
             for(int j=i+1;j>0;j--){
                 if(arr[j-1]>arr[j]){
-                    int temp=arr[i];
-                    arr[i]=arr[j];
-                    arr[j]=arr[i];
+                    int temp=arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=temp;
                 }else{
                     break;
                 }
