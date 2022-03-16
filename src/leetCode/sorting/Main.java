00a34366cad3f05069cbcaf2f880235a63f2732c
Prabesh <prabesh.bista369@gmail.com>
2022-03-16 20:29:01 +0545

    268. Missing Number

---
+++ src/leetCode/sorting/Main.java
@@ -0,0 +1,7 @@
+package leetCode.sorting;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
--- src/leetCode/sorting/MissingNumber.java
+++ src/leetCode/sorting/MissingNumber.java
@@ -2,7 +2,27 @@ package leetCode.sorting;
 
 public class MissingNumber {
     public int missingNumber(int[] nums) {
+        int i=0;
+        while(i<nums.length){
+            if(i!=nums[i]){
+                int temp=nums[i];
+                if(nums[i]>nums.length-1){
+                    nums[i]=nums[temp-1];
+                    nums[temp-1]=temp;
+                }
+                nums[i]=nums[temp-1];
+            }else{
+                i++;
+            }
+        }
 
+        int missingNumber=nums.length;
+        for(int j=0;j<nums.length;j++){
+            if(j!=nums[j]){
+                missingNumber=j;
+            }
+        }
+        return missingNumber;
     }
 
 }
