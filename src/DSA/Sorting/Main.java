351621c695105a549ea05eddb6d0381f5ff7a165
Prabesh <prabesh.bista369@gmail.com>
2022-03-16 19:08:29 +0545

    Cyclic Sort Algorithm

--- src/DSA/Sorting/Main.java
+++ src/DSA/Sorting/Main.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr={5,4,3,2,1};
+        int[] arr={5,4,3,2,6,1};
 //        BubbleSort bubbleSort=new BubbleSort();
 //        int[] newArr=bubbleSort.bubbleSort(arr);
 //        System.out.println(Arrays.toString(newArr));
