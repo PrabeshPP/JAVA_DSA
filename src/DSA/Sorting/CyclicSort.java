9cf4be4964f80a41df77e1f6e6526791c88e173f
Prabesh <prabesh.bista369@gmail.com>
2022-03-16 19:06:37 +0545

    Cyclic Sort Algorithm

--- src/DSA/Sorting/CyclicSort.java
+++ src/DSA/Sorting/CyclicSort.java
@@ -1,4 +1,23 @@
 package DSA.Sorting;
 
+
+//Cyclic Sort Algorithm
 public class CyclicSort {
+    int[] cyclicSort(int[] nums){
+        int i=0;
+        while(i<nums.length){
+            if(i+1!=nums[i]){
+                int temp=nums[i];
+                 nums[i]=nums[temp-1];
+                 nums[temp-1]=temp;
+
+            }else{
+                i++;
+            }
+        }
+
+        return nums;
+    }
+
+
 }
