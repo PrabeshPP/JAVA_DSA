33c2ea5ad27c8dc27e25d81caf84c3d8257bbd76
Prabesh <prabesh.bista369@gmail.com>
2022-03-17 19:34:37 +0545

    1608. Special Array With X Elements Greater Than or Equal X

--- src/leetCode/search/SpecialArray.java
+++ src/leetCode/search/SpecialArray.java
@@ -1,4 +1,26 @@
 package leetCode.search;
 
 public class SpecialArray {
+    public int specialArray(int[] nums) {
+        int x=0;
+        int y=0;
+        for (int i=0;i<=nums.length;i++){
+            for(int j=0;j< nums.length;j++){
+                if(i>=nums[j]){
+                    x++;
+                }
+            }
+
+            if(x!=i){
+                x=-1;
+            }
+
+
+        }
+
+        return x;
+
+    }
+
+
 }
