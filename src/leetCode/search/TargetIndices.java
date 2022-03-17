e15c8d290df5b6522b77c5dcdc88d1525085fa9b
Prabesh <prabesh.bista369@gmail.com>
2022-03-17 20:57:55 +0545

    2089. Find Target Indices After Sorting Array

--- src/leetCode/search/TargetIndices.java
+++ src/leetCode/search/TargetIndices.java
@@ -9,9 +9,41 @@ public class TargetIndices {
     public List<Integer> targetIndices(int[] nums, int target) {
         List<Integer> list=new ArrayList<Integer>();
         Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
+        int index1=binarySearch(nums,target,true);
+        int index2=binarySearch(nums,target,false);
+        for(int i=index1;i<=index2;i++){
+            list.add(i);
+        }
+
 
 
         return list;
     }
+
+    int binarySearch(int[] nums,int target,boolean isLeft){
+        int start=0;
+        int end=nums.length;
+        int index=-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target){
+                if(isLeft){
+                    end=mid-1;
+
+
+                }else{
+                    start=mid+1;
+
+                }
+                index=mid;
+
+            }else if(nums[mid]>target){
+                end=mid-1;
+            }else {
+                start=mid+1;
+            }
+        }
+        return index;
+    }
+
 }
