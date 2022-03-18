c6af58bc919e2b972410b1b475e1dac310d40e6b
Prabesh <prabesh.bista369@gmail.com>
2022-03-18 21:31:33 +0545

    448. Find All Numbers Disappeared in an Array(Google Question)

--- src/leetCode/sorting/NumbersDisappeared.java
+++ src/leetCode/sorting/NumbersDisappeared.java
@@ -1,4 +1,15 @@
 package leetCode.sorting;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NumbersDisappeared {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> list=new ArrayList<Integer>();
+
+
+
+
+        return  list;
+    }
 }
