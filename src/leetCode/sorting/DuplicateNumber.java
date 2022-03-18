5a27ac2cdee3c65a7201d694f15678cb663480dd
Prabesh <prabesh.bista369@gmail.com>
2022-03-18 22:15:37 +0545

    287. Find the Duplicate Number

--- src/leetCode/sorting/DuplicateNumber.java
+++ src/leetCode/sorting/DuplicateNumber.java
@@ -1,7 +1,27 @@
 package leetCode.sorting;
 
+import java.util.Arrays;
+
 public class DuplicateNumber {
     public int findDuplicate(int[] nums) {
+        int number=-1;
+        //
+        int i=0;
+        while(i<nums.length){
+            if(nums[i]!=nums[nums[i]-1]){
+                int temp=nums[i];
+                nums[i]=nums[temp-1];
+                nums[temp-1]=temp;
+            }
+        }
+
+        System.out.println(Arrays.toString(nums));
+
+
+
+
+
+        return number;
 
     }
 }
