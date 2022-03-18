47b44019d1f3e2853cdff1f88c69d9743424fd67
Prabesh <prabesh.bista369@gmail.com>
2022-03-18 22:17:43 +0545

    287. Find the Duplicate Number

--- src/leetCode/sorting/Main.java
+++ src/leetCode/sorting/Main.java
@@ -5,13 +5,15 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr={1,1};
+        int[] arr={1,3,4,2,2};
 //        MissingNumber missingNumber=new MissingNumber();
 //        int result=missingNumber.missingNumber(arr);
 //        System.out.println(result);
 //        NumbersDisappeared numbersDisappeared=new NumbersDisappeared();
 //        List<Integer> list=numbersDisappeared.findDisappearedNumbers(arr);
 //        System.out.println(Arrays.toString(list.toArray()));
+
+        DuplicateNumber duplicateNumber=new DuplicateNumber();
 
 
 
