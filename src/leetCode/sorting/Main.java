3c8326a415b922f1583ac8bac92b0f9d92c5c06b
Prabesh <prabesh.bista369@gmail.com>
2022-03-20 21:05:46 +0545

    41. First Missing Positive

--- src/leetCode/sorting/Main.java
+++ src/leetCode/sorting/Main.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr={1,2,0};
+        int[] arr={7,8,9,11,12};
 //        MissingNumber missingNumber=new MissingNumber();
 //        int result=missingNumber.missingNumber(arr);
 //        System.out.println(result);
