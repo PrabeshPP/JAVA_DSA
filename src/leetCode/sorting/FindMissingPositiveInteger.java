a38ea7823848ef5a3c0465add7239ba7de4eefc7
Prabesh <prabesh.bista369@gmail.com>
2022-03-20 21:05:00 +0545

    41. First Missing Positive

--- src/leetCode/sorting/FindMissingPositiveInteger.java
+++ src/leetCode/sorting/FindMissingPositiveInteger.java
@@ -6,15 +6,26 @@ public class FindMissingPositiveInteger {
     public int firstMissingPositive(int[] nums) {
         int i=0;
         while(i<nums.length){
-            if(nums[i]>0 &&nums[i]!=nums[nums[i-1]] && nums[i]<nums.length){
-                int temp=nums[i];
-                nums[i]=nums[temp-1];
-                nums[temp-1]=temp;
+            if(nums[i]>0 &&  nums[i]<=nums.length && nums[i]!=nums[nums[i]-1] ){
+
+                    int temp=nums[i];
+                    nums[i]=nums[temp-1];
+                    nums[temp-1]=temp;
+
+
+
             }else{
                 i++;
             }
         }
-        System.out.println(Arrays.toString(nums));
+
+        for(int j=0;j<nums.length;j++){
+            if(nums[j]!=j+1){
+                return  j+1;
+            }
+        }
+
+
         return 1;
 
 
