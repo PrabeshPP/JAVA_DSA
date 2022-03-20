277de8f03e620aa0839d0036b95fe4446257a83c
Prabesh <prabesh.bista369@gmail.com>
2022-03-20 20:29:14 +0545

    442. Find All Duplicates in an Array

--- src/leetCode/sorting/AllDuplicatesArray.java
+++ src/leetCode/sorting/AllDuplicatesArray.java
@@ -1,7 +1,34 @@
 package leetCode.sorting;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AllDuplicatesArray {
     public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> list= new ArrayList<Integer>();
+
+        int i=0;
+
+        while(i<nums.length){
+            if(nums[i]!=nums[nums[i]-1]){
+                int temp=nums[i];
+                nums[i]=nums[temp-1];
+                nums[temp-1]=temp;
+            }else{
+                i++;
+            }
+        }
+
+
+        for(int j=0;j<nums.length;j++){
+            if(nums[j]!=j+1){
+                list.add(nums[j]);
+            }
+
+        }
+
+
+        return list;
 
     }
 }
