2e9cc6e2c4a2ccf2c4c611ce8431795ca604e87a
Prabesh <prabesh.bista369@gmail.com>
2022-03-20 20:31:53 +0545

    442. Find All Duplicates in an Array

--- src/leetCode/sorting/Main.java
+++ src/leetCode/sorting/Main.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr={3,1,3,2};
+        int[] arr={4,3,2,7,8,2,3,1};
 //        MissingNumber missingNumber=new MissingNumber();
 //        int result=missingNumber.missingNumber(arr);
 //        System.out.println(result);
