be3387a548572e745ab66be58613406acd9a245d
Prabesh <prabesh.bista369@gmail.com>
2022-03-21 17:51:40 +0545

    Strings  and StringBuilder

--- src/Strings/Main.java
+++ src/Strings/Main.java
@@ -2,6 +2,9 @@ package Strings;
 
 public class Main {
     public static void main(String[] args) {
+        String a="Prabesh";
+        String b="Prabesh";
+        System.out.println(a==b);
 
     }
 }
