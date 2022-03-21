4385740730aec972d760c5a2f906ed749161762f
Prabesh <prabesh.bista369@gmail.com>
2022-03-21 18:30:02 +0545

    Strings  and StringBuilder

--- src/Strings/Main.java
+++ src/Strings/Main.java
@@ -3,8 +3,10 @@ package Strings;
 public class Main {
     public static void main(String[] args) {
         String a="Prabesh";
-        String b="Prabesh";
+        //It will create a new obj ,Out-Side of the string pool
+        String b=new String("Prabesh");
         System.out.println(a==b);
+
 
     }
 }
