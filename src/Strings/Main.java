ac89fa1883ff17de7e04fbf575f1f7e1ede90574
Prabesh <prabesh.bista369@gmail.com>
2022-03-26 18:24:23 +0545

    String Builder in Java

--- src/Strings/Main.java
+++ src/Strings/Main.java
@@ -2,11 +2,7 @@ package Strings;
 
 public class Main {
     public static void main(String[] args) {
-        String a="Prabesh";
-        //It will create a new obj ,Out-Side of the string pool
-        String b=new String("Prabesh");
-        System.out.println(a==b);
-
+        //String Builder
 
     }
 }
