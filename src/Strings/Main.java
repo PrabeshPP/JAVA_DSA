25bf7d1c81131686c8f847916f957b3b187d5fc7
Prabesh <prabesh.bista369@gmail.com>
2022-03-26 18:27:46 +0545

    String Builder in Java

--- src/Strings/Main.java
+++ src/Strings/Main.java
@@ -3,6 +3,7 @@ package Strings;
 public class Main {
     public static void main(String[] args) {
         //String Builder
+        //StringBuilder will not create a new Obj every time a new value is added or modified like strings
         StringBuilder builder=new StringBuilder();
         for(int i=0;i<26;i++){
             char ch=(char)('a'+i);
