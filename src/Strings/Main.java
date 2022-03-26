8e046f401d2dd67c46c3a8b1ba0bf3d72abe5963
Prabesh <prabesh.bista369@gmail.com>
2022-03-26 18:30:08 +0545

    String Builder in Java

--- src/Strings/Main.java
+++ src/Strings/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         //String Builder
         //StringBuilder will not create a new Obj every time a new value is added or modified like strings
+        //StringBuilder is mutable
         StringBuilder builder=new StringBuilder();
         for(int i=0;i<26;i++){
             char ch=(char)('a'+i);
@@ -11,6 +12,7 @@ public class Main {
 
         }
 
+      
         System.out.println(builder);
 
     }
