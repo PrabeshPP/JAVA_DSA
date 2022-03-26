a9160fca2cd31d6fd903c80dd26647058488a698
Prabesh <prabesh.bista369@gmail.com>
2022-03-26 18:47:09 +0545

    131. Palindrome Partitioning

---
+++ src/leetCode/Strings/Main.java
@@ -0,0 +1,12 @@
+package leetCode.Strings;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        String s="aab";
+        PalindromePartitioning palindromePartitioning=new PalindromePartitioning();
+        List<List<String>> listResult=palindromePartitioning.partition(s);
+
+    }
+}
