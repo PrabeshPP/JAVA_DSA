6c062a219ac57304a820f5309b1e53631370009e
Prabesh <prabesh.bista369@gmail.com>
2022-03-26 18:45:58 +0545

    131. Palindrome Partitioning

--- src/leetCode/Strings/PalindromePartitioning.java
+++ src/leetCode/Strings/PalindromePartitioning.java
@@ -1,7 +1,24 @@
 package leetCode.Strings;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class PalindromePartitioning {
     public List<List<String>> partition(String s) {
+        StringBuilder builder=new StringBuilder();
+        List<List<String>> list= new ArrayList<>();
+        List<String> listStr=new ArrayList<String>();
+        for(int i=0;i<s.length();i++){
+            Character chr=s.charAt(i);
+            listStr.add(chr.toString());
+
+        }
+
+        list.add(listStr);
+        System.out.println(Arrays.toString(list.toArray()));
+
+        return list;
 
     }
 }
