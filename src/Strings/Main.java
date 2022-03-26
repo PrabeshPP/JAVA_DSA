e6fb71f5f66f8c0af6672fae950656c833dee4de
Prabesh <prabesh.bista369@gmail.com>
2022-03-26 18:26:52 +0545

    String Builder in Java

--- src/Strings/Main.java
+++ src/Strings/Main.java
@@ -3,6 +3,14 @@ package Strings;
 public class Main {
     public static void main(String[] args) {
         //String Builder
+        StringBuilder builder=new StringBuilder();
+        for(int i=0;i<26;i++){
+            char ch=(char)('a'+i);
+            builder.append(ch);
+
+        }
+
+        System.out.println(builder);
 
     }
 }
