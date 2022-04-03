83bdd1742d7cd93c7e0cbfe0a1fbbf5bb0d15983
Prabesh <prabesh.bista369@gmail.com>
2022-04-03 19:31:03 +0545

    Patterns Question in Java

--- src/DSA/Patterns/Main.java
+++ src/DSA/Patterns/Main.java
@@ -5,8 +5,7 @@ import java.awt.*;
 public class Main {
     public static void main(String[] args) {
         //Basic of Patterns in java
-        pattern1(5);
-//        pattern2(5);
+        pattern4(5);
 
     }
 
@@ -20,8 +19,8 @@ public class Main {
     }
 
     static void pattern2(int n){
-        for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++){
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=i;j++){
                 System.out.print("*");
             }
             System.out.println();
@@ -29,4 +28,21 @@ public class Main {
 
     }
 
+    static void pattern3(int n){
+        for(int i=0;i<=n;i++){
+            for(int j=n-i;j>=0;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern4(int n){
+        for (int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
 }
