7d53f1dca622d4a767632349073698a6854c9716
Prabesh <prabesh.bista369@gmail.com>
2022-04-03 19:10:59 +0545

    Patterns in Java

--- src/DSA/Patterns/Main.java
+++ src/DSA/Patterns/Main.java
@@ -5,7 +5,7 @@ import java.awt.*;
 public class Main {
     public static void main(String[] args) {
         for(int i=0;i<5;i++){
-            for(int j=0;j<i;j++){
+            for(int j=5-i;j>0;j--){
                 System.out.print("*");
             }
             System.out.println();
