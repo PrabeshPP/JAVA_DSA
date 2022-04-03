e72e4f111a7739f3669632b29ee8a366ea60cfb5
Prabesh <prabesh.bista369@gmail.com>
2022-04-03 19:25:50 +0545

    Patterns Question in Java

--- src/DSA/Patterns/Main.java
+++ src/DSA/Patterns/Main.java
@@ -5,8 +5,18 @@ import java.awt.*;
 public class Main {
     public static void main(String[] args) {
         //Basic of Patterns in java
-        pattern2(5);
+        pattern1(5);
+//        pattern2(5);
 
+    }
+
+    static void pattern1(int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.println("*");
+            }
+            System.out.println();
+        }
     }
 
     static void pattern2(int n){
