084205f4139bfe31a235d3f92419d0020a153523
Prabesh <prabesh.bista369@gmail.com>
2022-04-03 19:24:04 +0545

    pattern number 2 questions solved

--- src/DSA/Patterns/Main.java
+++ src/DSA/Patterns/Main.java
@@ -5,12 +5,18 @@ import java.awt.*;
 public class Main {
     public static void main(String[] args) {
         //Basic of Patterns in java
-        for(int i=0;i<10;i++){
+        pattern2(5);
+
+    }
+
+    static void pattern2(int n){
+        for(int i=0;i<n;i++){
             for(int j=0;j<i;j++){
                 System.out.print("*");
             }
             System.out.println();
         }
+
     }
 
 }
