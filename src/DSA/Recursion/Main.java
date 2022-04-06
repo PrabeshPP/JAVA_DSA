2b90f520b4c605e0dad114c848c2d22e86b42ba9
Prabesh <prabesh.bista369@gmail.com>
2022-04-06 17:44:24 +0545

    Recursion in Java

--- src/DSA/Recursion/Main.java
+++ src/DSA/Recursion/Main.java
@@ -2,6 +2,16 @@ package DSA.Recursion;
 
 public class Main {
     public static void main(String[] args) {
+        //write a function that prints hello world
+        message(5);
 
+    }
+
+    static void message(int times){
+        if(times==0){
+            return;
+        }
+        System.out.println("Hello World!");
+        message(times-1);
     }
 }
