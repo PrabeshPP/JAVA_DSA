b57ab83b2bb830a3b5ace90e4bca509c5c59aa5d
Prabesh <prabesh.bista369@gmail.com>
2022-04-07 18:28:19 +0545

    BinarySearch using Recursion in Java

--- src/DSA/Recursion/BinarySearch.java
+++ src/DSA/Recursion/BinarySearch.java
@@ -3,7 +3,21 @@ package DSA.Recursion;
 public class BinarySearch {
     //Binary Search Using Recursion in Java
     //Divide and Conquer Relation
-    int search(int[] arr,int target){
+    int search(int[] arr,int target,int start,int end){
+        if(start>end){
+            return -1;
+        }
+        int mid=start+(end-start)/2;
+        if(arr[mid]==target){
+            return mid;
+        }else if(arr[mid]>target){
+            end=mid-1;
+        }else{
+            start=mid+1;
+        }
+
+        search(arr,target,start,end);
+
 
 
         return -1;
