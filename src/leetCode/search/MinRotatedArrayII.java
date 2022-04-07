3655ca6391fae739126617687264ae4850e2f820
Prabesh <prabesh.bista369@gmail.com>
2022-04-07 20:49:20 +0545

    154. Find Minimum in Rotated Sorted Array II

---
+++ src/leetCode/search/MinRotatedArrayII.java
@@ -0,0 +1,47 @@
+package leetCode.search;
+
+public class MinRotatedArrayII {
+    public int findMin(int[] nums) {
+        if(nums[0]>nums[nums.length-1]){
+            return nums[nums.length-1];
+        }
+        if(nums[0]<nums[nums.length-1]){
+            return nums[0];
+
+        }
+
+        return nums[pivotPoint(nums)+1];
+
+
+
+    }
+
+    int pivotPoint(int[] nums){
+        int start=0;
+        int end=nums.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(mid>start && nums[mid]<nums[mid-1]){
+                return mid-1;
+            }
+            if(mid<end && nums[mid+1]<nums[mid]){
+                return  mid;
+
+            }
+
+            if(nums[start]==nums[mid]){
+                start=mid+1;
+            }
+
+            if(nums[start]<nums[mid]){
+                start=mid+1;
+            }
+            if(nums[start]>nums[mid]){
+                end=mid-1;
+            }
+        }
+
+        return -1;
+
+    }
+}
