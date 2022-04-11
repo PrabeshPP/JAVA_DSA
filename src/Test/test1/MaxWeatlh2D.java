f975147d0fa7850a40ece2abd676dbdc05624d9a
Prabesh <prabesh.bista369@gmail.com>
2022-04-11 21:22:40 +0545

    154. Find Minimum in Rotated Sorted Array II

--- src/Test/test1/MaxWeatlh2D.java
+++ src/Test/test1/MaxWeatlh2D.java
@@ -1,27 +1,26 @@
 package Test.test1;
 
+import java.util.Scanner;
+
 public class MaxWeatlh2D {
     public static void main(String[] args) {
-        int[][] nums = {
-                {1,4,6},
-                {1,2,3}
-        };
-        System.out.println(wealth(nums));
+        float var1 ,var2;
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter Number 1:");
+        var1=scanner.nextFloat();
+        System.out.println("Enter Number 2:");
+        var2=scanner.nextFloat();
+        if(var1 %2 ==1 && var2%2==1){
+            System.out.println(var1-var2);
+        }else if(var1 %2 ==0 && var2 %2 ==0){
+            System.out.println(var1+var2);
+        }else{
+            if(var1>var2){
+                System.out.println(var1/var2);
 
-    }
-    static int wealth(int[][] nums){
-
-        int maxSum = 0;
-
-        for(int rol = 0; rol < nums.length; rol++) {
-            int sum = 0;
-            for (int col = 0; col < nums[rol].length; col++){
-                sum += nums[rol][col];
-            }
-            if (maxSum < sum) {
-                maxSum = sum;
+            }else{
+                System.out.println(var2/var1);
             }
         }
-        return maxSum;
     }
 }
