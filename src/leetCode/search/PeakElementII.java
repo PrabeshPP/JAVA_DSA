50ec511daec6e4a7c03e568a817a7b52d3dc8b94
Prabesh <prabesh.bista369@gmail.com>
2022-04-12 18:23:08 +0545

    1901. Find a Peak Element II

--- src/leetCode/search/PeakElementII.java
+++ src/leetCode/search/PeakElementII.java
@@ -3,8 +3,29 @@ package leetCode.search;
 public class PeakElementII {
     public int[] findPeakGrid(int[][] mat) {
         int[] arr=new int[2];
+            for(int i=0;i<mat.length;i++){
+                int peak=peakPoint(i,mat);
+                System.out.println(mat[i][peak]);
+
+            }
 
         return arr;
+
+    }
+
+    int peakPoint(int i,int[][] mat){
+        int start=0;
+        int end=mat[i].length-1;
+        while(start<end){
+            int mid=start+(end-start)/2;
+            if(mat[i][mid]>mat[i][mid+1]){
+                end=mid;
+            }else{
+                start=mid+1;
+            }
+        }
+
+        return start;
 
     }
 }
