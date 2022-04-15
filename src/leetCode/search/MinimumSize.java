cc9847f590bf71295fd3c474b381e0b745582b8a
Prabesh <prabesh.bista369@gmail.com>
2022-04-15 21:39:57 +0545

    209. Minimum Size Subarray Sum

---
+++ src/leetCode/search/MinimumSize.java
@@ -0,0 +1,21 @@
+package leetCode.search;
+
+public class MinimumSize {
+    public static void main(String[] args) {
+        int[] arr={2,3,1,2,4,3};
+        int target=7;
+        int len=minSubArrayLen(target,arr);
+
+    }
+
+    static int minSubArrayLen(int target, int[] nums) {
+        int size=0;
+
+
+        return size;
+
+    }
+
+}
+
+
