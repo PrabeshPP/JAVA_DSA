81907650a2da3951bbe557faf1b23edb6fd2c622
Prabesh <prabesh.bista369@gmail.com>
2022-04-15 16:19:53 +0545

    Insertion Sort revisited

--- src/DSA/Sorting/InsertionSort.java
+++ src/DSA/Sorting/InsertionSort.java
@@ -2,6 +2,7 @@ package DSA.Sorting;
 
 public class InsertionSort {
     //Insertion Sort
+    //
     int[] insertionsort(int[] arr){
         for(int i=0;i<arr.length-1;i++){
             for(int j=i+1;j>0;j--){
