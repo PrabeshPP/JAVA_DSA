5346868db1ac6153b0170d606e8a9d603a09aba4
Prabesh <prabesh.bista369@gmail.com>
2022-04-17 19:04:33 +0545

    169. Majority Element

--- src/leetCode/sorting/MajorityElement.java
+++ src/leetCode/sorting/MajorityElement.java
@@ -4,40 +4,46 @@ import java.util.Arrays;
 
 public class MajorityElement {
     public static void main(String[] args) {
-        int[] arr=new int[]{2,2,1,1,1,2,2};
-        int result=majorityElement(arr);
-
+        int[] arr = new int[]{3,1,3};
+        int result = majorityElement(arr);
+        System.out.println(result);
 
 
     }
-    static int majorityElement(int[] nums) {
-        nums=sort(nums);
-        System.out.println(Arrays.toString(nums));
-        int maxElem=0;
-        int maxCount=0;
-        int start=0;
-        int counter=0;
-        for(int i=start;i<nums.length;i++){
-            int elem=nums[i];
-            for(int j=i+1;j<nums.length;j++){
-                if(elem==nums[j]){
-                    counter++;
 
-                }else{
-                    start=j;
+    static int majorityElement(int[] nums) {
+        nums = sort(nums);
+        System.out.println(Arrays.toString(nums));
+        int maxElem = 0;
+        int maxCount = 0;
+        int start = 0;
+        int counter = 0;
+        while (start <= nums.length - 1){
+            int elem = nums[start];
+            for (int j = start + 1; j < nums.length; j++) {
+                if (elem == nums[j]) {
+                    counter++;
+                    start++;
+                } else {
+                    start = j;
                     break;
 
                 }
-            }
-
-            if(counter>maxCount){
-                maxCount=counter;
-                maxElem=elem;
 
             }
+            System.out.println(start);
+            if (counter > maxCount) {
+                maxCount = counter;
+                maxElem = elem;
+
+            }
+            start++;
+
         }
 
-    return 1;
+
+
+    return maxElem;
     }
 
    static int[] sort(int[] nums){
