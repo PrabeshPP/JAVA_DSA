05422f0f138afc2f28a0799d0b10c25634c9bd2b
Prabesh <prabesh.bista369@gmail.com>
2022-04-18 20:36:35 +0545

    1011. Capacity To Ship Packages Within D Days

--- src/leetCode/search/Shippackages.java
+++ src/leetCode/search/Shippackages.java
@@ -4,9 +4,32 @@ public class Shippackages {
     public int shipWithinDays(int[] weights, int days) {
         int max=0;
         int totalWeight=0;
+        int ans=0;
         for(int i=0;i<weights.length;i++){
-
+            totalWeight+=weights[i];
+            max=Math.max(max,weights[i]);
         }
+
+        if(days==weights.length){
+            return max;
+        }
+
+        int start=max;
+        int end=totalWeight;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(isWeightLessOrEqual(mid,days)){
+                ans=mid;
+                end=mid-1;
+            }else{
+                start=mid+1;
+            }
+        }
+        return ans;
+    }
+
+    public boolean isWeightLessOrEqual(int maxCapacity,int days){
+
 
     }
 }
