c529ef985678330689ccb7beef2177472c132c99
Prabesh <prabesh.bista369@gmail.com>
2022-04-19 19:23:53 +0545

    136. Single Number

--- src/leetCode/array/SingleNumber.java
+++ src/leetCode/array/SingleNumber.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class SingleNumber {
     public int singleNumber(int[] nums) {
         Arrays.sort(nums);
+        System.out.println(Arrays.toString(nums));
         for(int i=0;i<nums.length;i++){
             int currentElem=nums[i];
             int counter=1;
@@ -14,6 +15,7 @@ public class SingleNumber {
                 }
 
             }
+            System.out.println(counter);
             if(counter==1){
                 return currentElem;
             }
