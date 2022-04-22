2607f306ab11feb06adfc7df00fe544deb7f3844
Prabesh <prabesh.bista369@gmail.com>
2022-04-22 17:55:14 +0545

    reverse the string

--- src/leetCode/Strings/Main.java
+++ src/leetCode/Strings/Main.java
@@ -10,8 +10,13 @@ public class Main {
 //        List<List<String>> listResult=palindromePartitioning.partition(s);
 //        System.out.println(Arrays.toString(listResult.toArray()));
 
-        IPAddress ipAddress=new IPAddress();
-        String address=ipAddress.defangIPaddr("1.1.1.1");
-        System.out.println(address);
+//        IPAddress ipAddress=new IPAddress();
+//        String address=ipAddress.defangIPaddr("1.1.1.1");
+//        System.out.println(address);
+
+        char[] string={'p','r','a','b','e','s'};
+        ReverseString reverseString=new ReverseString();
+        char[] result=reverseString.reverseString(string);
+        System.out.print(Arrays.toString(result));
     }
 }
---
+++ src/leetCode/Strings/ReverseString.java
@@ -0,0 +1,21 @@
+package leetCode.Strings;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReverseString {
+
+   public char[] reverseString(char[] string){
+       int i=0;
+       int j=string.length-1;
+       while(i<=j){
+           char temp=string[i];
+           string[i]=string[j];
+           string[j]=temp;
+           i++;
+           j--;
+       }
+       return string;
+   }
+
+}
