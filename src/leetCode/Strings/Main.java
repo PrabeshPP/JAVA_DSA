9c73b5d676a90efdec24d7b370ec254b1ed0e489
Prabesh <prabesh.bista369@gmail.com>
2022-04-23 19:40:11 +0545

    48. Rotate Image

--- src/leetCode/Strings/Main.java
+++ src/leetCode/Strings/Main.java
@@ -1,5 +1,7 @@
 package leetCode.Strings;
 
+import leetCode.array.RotateImage;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -14,9 +16,16 @@ public class Main {
 //        String address=ipAddress.defangIPaddr("1.1.1.1");
 //        System.out.println(address);
 
-        char[] string={'a','b','c'};
-        ReverseString reverseString=new ReverseString();
-        char[] result=reverseString.reverseString(string);
-        System.out.print(Arrays.toString(result));
+//        char[] string={'a','b','c'};
+//        ReverseString reverseString=new ReverseString();
+//        char[] result=reverseString.reverseString(string);
+//        System.out.print(Arrays.toString(result));
+        int[][] matrix={
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+        RotateImage rotateImage=new RotateImage();
+        rotateImage.rotate(matrix);
     }
 }
--- src/leetCode/array/RotateImage.java
+++ src/leetCode/array/RotateImage.java
@@ -1,13 +1,19 @@
 package leetCode.array;
 
+import java.util.Arrays;
+
 public class RotateImage {
     public void rotate(int[][] matrix) {
-
+        //transpose the matrix
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j< matrix[i].length;j++){
-
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
             }
         }
+
+
 
     }
 
