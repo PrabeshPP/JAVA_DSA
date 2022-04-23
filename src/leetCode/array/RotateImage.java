3ef1d3b3d42743bc74bc5987d7da645aba8dfcad
Prabesh <prabesh.bista369@gmail.com>
2022-04-23 19:27:41 +0545

    48. Rotate Image

--- src/leetCode/array/RotateImage.java
+++ src/leetCode/array/RotateImage.java
@@ -1,5 +1,14 @@
 package leetCode.array;
 
 public class RotateImage {
+    public void rotate(int[][] matrix) {
+
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j< matrix[i].length;j++){
+
+            }
+        }
+
+    }
 
 }
