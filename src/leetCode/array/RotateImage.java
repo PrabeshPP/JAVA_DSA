e0506817ecbcba168ba51ebe5c2e2e2126a87499
Prabesh <prabesh.bista369@gmail.com>
2022-04-23 20:04:28 +0545

    48. Rotate Image(space complexity constant)

--- src/leetCode/array/RotateImage.java
+++ src/leetCode/array/RotateImage.java
@@ -27,12 +27,6 @@ public class RotateImage {
 
         }
 
-        for(int i=0;i< matrix.length;i++){
-            for(int j=0;j< matrix.length;j++){
-                System.out.print(matrix[i][j]);
-            }
-            System.out.println();
-        }
 
 
 
