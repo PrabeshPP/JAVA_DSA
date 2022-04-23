31bbe4f337a1899fd2c32b784b872357c30a3001
Prabesh <prabesh.bista369@gmail.com>
2022-04-23 19:49:40 +0545

    48. Rotate Image

--- src/leetCode/array/RotateImage.java
+++ src/leetCode/array/RotateImage.java
@@ -6,11 +6,28 @@ public class RotateImage {
     public void rotate(int[][] matrix) {
         //transpose the matrix
         for(int i=0;i<matrix.length;i++){
-            for(int j=0;j< matrix[i].length;j++){
+            for(int j=i;j< matrix[i].length;j++){
+
                 int temp=matrix[i][j];
                 matrix[i][j]=matrix[j][i];
                 matrix[j][i]=temp;
             }
+
+        }
+
+        //now swapping the element to rotate the image
+
+        for(int i=0;i< matrix.length;i++){
+            for(int j=0;j<matrix.length;j++) {
+
+            }
+        }
+
+        for(int i=0;i< matrix.length;i++){
+            for(int j=0;j< matrix.length;j++){
+                System.out.print(matrix[i][j]);
+            }
+            System.out.println();
         }
 
 
