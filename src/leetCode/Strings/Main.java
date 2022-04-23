6930a1f13c2da36300873344d4392c906a67e4e6
Prabesh <prabesh.bista369@gmail.com>
2022-04-23 20:03:37 +0545

    48. Rotate Image

--- src/leetCode/Strings/Main.java
+++ src/leetCode/Strings/Main.java
@@ -21,9 +21,10 @@ public class Main {
 //        char[] result=reverseString.reverseString(string);
 //        System.out.print(Arrays.toString(result));
         int[][] matrix={
-                {1,2,3},
-                {4,5,6},
-                {7,8,9}
+                {5,1,9,11},
+                {2,4,8,10},
+                {13,3,6,7},
+                {15,14,12,16}
         };
         RotateImage rotateImage=new RotateImage();
         rotateImage.rotate(matrix);
--- src/leetCode/array/RotateImage.java
+++ src/leetCode/array/RotateImage.java
@@ -18,9 +18,13 @@ public class RotateImage {
         //now swapping the element to rotate the image
 
         for(int i=0;i< matrix.length;i++){
-            for(int j=0;j<matrix.length;j++) {
+            for(int j=0;j<matrix.length/2;j++) {
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[i][matrix[i].length-1-j];
+                matrix[i][matrix[i].length-1-j]=temp;
 
             }
+
         }
 
         for(int i=0;i< matrix.length;i++){
