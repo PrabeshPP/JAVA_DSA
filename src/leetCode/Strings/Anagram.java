3b2d6231f0f1cc2fc79e08a87559ea1b7ae182e5
Prabesh <prabesh.bista369@gmail.com>
2022-04-24 18:37:38 +0545

    242. Valid Anagram

--- src/leetCode/Strings/Anagram.java
+++ src/leetCode/Strings/Anagram.java
@@ -6,14 +6,18 @@ public class Anagram {
     public boolean isAnagram(String s, String t) {
 
         int[] a=new int[26];
-//       for(int i=0;i<s.length();i++) a[s.charAt(i)-'a']++;
-//
-//       for(int i=0;i<t.length();i++) a[s.charAt(i)-'a']--;
-        for (int i = 0; i < s.length(); i++) a[s.charAt(i) - 'a']++;
-        for (int i = 0; i < t.length(); i++) a[t.charAt(i) - 'a']--;
-
+       for(int i=0;i<s.length();i++){
+           a[s.charAt(i)-'a']++;
+       }
+       for(int i=0;i<t.length();i++){
+           a[t.charAt(i)-'a']--;
+       }
         System.out.println(Arrays.toString(a));
-       for(int i:a)if(i!=0)return false;
+       for(int i:a){
+           if(i!=0){
+               return false;
+           }
+       }
 
 
         return true;
