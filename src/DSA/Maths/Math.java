27298582bccc3fe096074d63ffa3ae0a15d18846
Prabesh <prabesh.bista369@gmail.com>
2022-04-25 19:46:58 +0545

    Odd or Even Using BitWise Operator

--- src/DSA/Maths/Math.java
+++ src/DSA/Maths/Math.java
@@ -3,16 +3,13 @@ package DSA.Maths;
 public class Math {
 
     public static void main(String[] args) {
-        oddOREven(19);
+        String result=oddOREven(10);
+        System.out.println(result);
 
 
     }
 
-    static void oddOREven(int Number){
-        if((Number&1)==1){
-            System.out.println("Odd");
-        }else{
-            System.out.println("Even");
-        }
+    static String oddOREven(int Number){
+        return (Number&1)==1?"Odd":"Even";
     }
 }
