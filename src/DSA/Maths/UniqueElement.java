5df859856e7bdccf7dd2e20fc90ee9b6a4d951c3
Prabesh <prabesh.bista369@gmail.com>
2022-04-25 19:57:00 +0545

    Find Unique Element using XOR Operator

--- src/DSA/Maths/UniqueElement.java
+++ src/DSA/Maths/UniqueElement.java
@@ -2,6 +2,11 @@ package DSA.Maths;
 
 public class UniqueElement {
     public static void main(String[] args) {
-
+        int[] arr={2,3,3,4,2,6,4};
+        int uniqueElement=0;
+        for(int i:arr){
+            uniqueElement^=i;
+        }
+        System.out.println(uniqueElement);
     }
 }
