8ac876144f30ec3276fce9cdc27d1b24a96aa7fa
Prabesh <prabesh.bista369@gmail.com>
2022-04-27 18:47:09 +0545

    Collections in Java

--- src/Collections/Arraylist.java
+++ src/Collections/Arraylist.java
@@ -1,4 +1,18 @@
 package Collections;
 
-public class Arraylist {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Arraylist<I extends Number> {
+    public static void main(String[] args) {
+        List<Integer> list= new ArrayList<Integer>();
+        list.add(20);
+        list.add(30);
+        list.add(60);
+        list.add(90);
+        list.add(100);
+        System.out.println(list);
+
+
+    }
 }
