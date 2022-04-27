447b40fea77eca296d2b33a87a7e52cf744e653d
Prabesh <prabesh.bista369@gmail.com>
2022-04-27 19:31:34 +0545

    Collections in Java

--- src/Collections/QueueInJava.java
+++ src/Collections/QueueInJava.java
@@ -1,4 +1,28 @@
 package Collections;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class QueueInJava {
+    public static void main(String[] args) {
+        //Queue in Java
+        Queue<Integer> queue=new LinkedList<Integer>();
+        queue.offer(10);
+        queue.offer(20);
+        queue.offer(30);
+        queue.offer(40);
+        queue.offer(50);
+        queue.offer(60);
+        //Printing the queue
+        System.out.println(queue);
+
+        //poll() function to return the element that would come first from the Queue
+        System.out.println(queue.poll());
+
+        //peek() function to show the head of the Queue
+        System.out.println(queue.peek());
+
+
+
+    }
 }
