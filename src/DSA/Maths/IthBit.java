ba760c92c538d29bd96d1c4f0c799cc8b1764e5f
Prabesh <prabesh.bista369@gmail.com>
2022-04-28 20:57:03 +0545

    217. Contains Duplicate

--- src/DSA/Maths/IthBit.java
+++ src/DSA/Maths/IthBit.java
@@ -2,7 +2,7 @@ package DSA.Maths;
 
 public class IthBit {
     public static void main(String[] args) {
-        int n=3;
+        int n=2;
         int number=20;
         System.out.println(number&(1<<n-1));
 
---
+++ src/leetCode/sorting/ContainsDuplicate.java
@@ -0,0 +1,20 @@
+package leetCode.sorting;
+
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        for(int i=0;i<nums.length;i++){
+            for(int j=i;j>0;j--) {
+                if(nums[j-1]>nums[j]){
+                    int temp=nums[j-1];
+                    nums[j-1]=nums[j];
+                    nums[j]=temp;
+                }
+
+            }
+        }
+        System.out.println(nums);
+        return false;
+    }
+
+
+}
