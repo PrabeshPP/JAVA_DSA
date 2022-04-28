aa156fcca34c8770914c8a8087f0bbbfbbdd1836
Prabesh <prabesh.bista369@gmail.com>
2022-04-28 20:43:45 +0545

    Ith bit using Java

--- src/DSA/Maths/IthBit.java
+++ src/DSA/Maths/IthBit.java
@@ -2,6 +2,10 @@ package DSA.Maths;
 
 public class IthBit {
     public static void main(String[] args) {
+        int n=3;
+        int number=20;
+        System.out.println(number&(1<<n-1));
+
 
     }
 }
