173369ee327dd57ee42af813f66d7c70052fe509
Prabesh <prabesh.bista369@gmail.com>
2022-05-01 18:51:20 +0545

    Hash Set

--- src/Collections/HashSetExample.java
+++ src/Collections/HashSetExample.java
@@ -1,7 +1,19 @@
 package Collections;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class HashSetExample {
     public static void main(String[] args) {
+        Set<Integer> set=new HashSet<>();
+        set.add(23);
+        set.add(40);
+        set.add(50);
+        set.add(56);
+        set.add(70);
+        set.add(75);
+
+        System.out.println(set);
 
     }
 }
