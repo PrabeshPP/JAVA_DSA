47ac847f4f775835022ba9f48cc283c6195030e1
Prabesh <prabesh.bista369@gmail.com>
2022-05-01 18:56:46 +0545

    LinkedHash Set

---
+++ src/Collections/LinkedListExample.java
@@ -0,0 +1,19 @@
+package Collections;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class LinkedListExample {
+    public static void main(String[] args) {
+        Set<Integer> set=new LinkedHashSet<>();
+        set.add(23);
+        set.add(40);
+        set.add(50);
+        set.add(56);
+        set.add(70);
+        set.add(75);
+
+        System.out.println(set.contains(20));
+        so
+    }
+}
