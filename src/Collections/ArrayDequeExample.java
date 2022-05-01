f39ce388cd72939bbe3bf08784ddc03c720604ce
Prabesh <prabesh.bista369@gmail.com>
2022-05-01 18:37:09 +0545

    ArrayDequeExample

--- src/Collections/ArrayDequeExample.java
+++ src/Collections/ArrayDequeExample.java
@@ -1,4 +1,25 @@
 package Collections;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class ArrayDequeExample {
+    public static void main(String[] args) {
+        ArrayDeque<Integer> arrayDeque=new ArrayDeque<>();
+        arrayDeque.offer(90);
+
+        // It will add the element in the from the first
+        arrayDeque.offerFirst(99);
+        //it will add the element from the last
+        arrayDeque.offerLast(120);
+        arrayDeque.offer(90);
+        //
+        System.out.println(arrayDeque);
+
+        //polling the element from the first
+        System.out.println(arrayDeque.pollFirst());
+
+        //polling the element from the last
+        System.out.println(arrayDeque.pollLast());
+    }
 }
