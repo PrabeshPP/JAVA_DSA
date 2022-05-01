20fbaad2eaa203eeaca9ad6623adc8bff5454138
Prabesh <prabesh.bista369@gmail.com>
2022-05-01 18:59:25 +0545

    Tree Set Example

--- src/Collections/TreeSetExample.java
+++ src/Collections/TreeSetExample.java
@@ -1,7 +1,19 @@
 package Collections;
 
+import java.util.Set;
+import java.util.TreeSet;
+
 public class TreeSetExample {
     public static void main(String[] args) {
+        //Tree set is used to store unique element and in sorted way
+        Set<Integer> set=new TreeSet<>();
+        set.add(23);
+        set.add(40);
+        set.add(50);
+        set.add(56);
+        set.add(70);
+
+        System.out.println(set);
 
     }
 }
