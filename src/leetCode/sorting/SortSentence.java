c5cf2cfd7b164f82c9be860f44d804853d104fc0
Prabesh <prabesh.bista369@gmail.com>
2022-05-01 18:11:09 +0545

    1859. Sorting the Sentence

--- src/leetCode/sorting/SortSentence.java
+++ src/leetCode/sorting/SortSentence.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class SortSentence {
     public static void main(String[] args) {
-        String s="Myself2 Me1 I4 and3";
+        String s="KTFkUVVrmYMSo2 wXlQraUqblfhCfDLK3 IfTuftYVualQ6 NhpQ5 HlRjClVtQrTFcwbx4 fi1";
         String o=sortSentence(s);
 
     }
@@ -12,14 +12,38 @@ public class SortSentence {
     public static  String sortSentence(String s) {
         StringBuilder str=new StringBuilder();
         String[] arr = s.split(" ");
-        for (int i = 0; i < arr.length; i++) {
-                char currentValue = arr[i].charAt(arr[i].length() - 1);
+        int j=0;
+        while(j<arr.length-1){
+                char currentValue = arr[j].charAt(arr[j].length() - 1);
                 int value = Integer.parseInt(Character.toString(currentValue));
-                String temp=arr[i].substring(0,arr[i].length());
-                arr[i]=arr[value-1];
-                arr[value-1]=temp;
+                char rValue=arr[value-1].charAt(arr[value-1].length()-1);
+                int rInt=Integer.parseInt(Character.toString(rValue));
+                if(value!=rInt){
+                    String temp=arr[j].substring(0,arr[j].length());
+                    arr[j]=arr[value-1];
+                    arr[value-1]=temp;
+
+                }else{
+                    j++;
+                }
+//
         }
 
-        return "Hello";
+
+        int i=0;
+        for(String elem:arr){
+            if(i== arr.length-1){
+                str.append(elem.substring(0, elem.length() - 1));
+            }else{
+                str.append(elem.substring(0, elem.length() - 1));
+                str.append(" ");
+            }
+
+
+            i++;
+        }
+
+
+        return str.toString();
     }
 }
