db3894520cef52dcc828e2f664217942667e9b90
Prabesh <prabesh.bista369@gmail.com>
2022-05-02 17:28:15 +0545

    nth magic number(Amazon Question)

--- src/DSA/Maths/MagicNumber.java
+++ src/DSA/Maths/MagicNumber.java
@@ -2,6 +2,17 @@ package DSA.Maths;
 
 public class MagicNumber {
     public static void main(String[] args) {
+        int n=2;
+        int ans=0;
+        int base=5;
+         while(n>0){
+             int last=n&1;
+             n=n>>1;
+             ans+=last*base;
+             base=5*base;
 
+
+         }
+        System.out.println(ans);
     }
 }
